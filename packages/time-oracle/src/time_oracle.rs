c9f43e147a633968b3d91b96be1926cf8c9064e7
v.kolesnikov <v.kolesnikov@nolus.io>
2022-03-07 15:43:26 +1000

    change the argument of query_global_time, simplify the tests

--- packages/time-oracle/src/time_oracle.rs
+++ packages/time-oracle/src/time_oracle.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Deps, Env, Response, StdResult, Storage, Timestamp};
+use cosmwasm_std::{Env, Response, StdResult, Storage, Timestamp};
 use cw_storage_plus::Item;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -16,54 +16,25 @@ pub fn update_global_time(storage: &mut dyn Storage, env: &Env) -> StdResult<Res
     Ok(Response::new().add_attribute("method", "update_time"))
 }
 
-pub fn query_global_time(deps: Deps) -> StdResult<GlobalTimeResponse> {
-    let time = GLOBAL_TIME.load(deps.storage)?;
+pub fn query_global_time(storage: &dyn Storage) -> StdResult<GlobalTimeResponse> {
+    let time = GLOBAL_TIME.load(storage)?;
     Ok(GlobalTimeResponse { time })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary, to_binary, Binary, DepsMut, MessageInfo};
-
-    // simplified version of smart contract
-    enum ExecuteMsg {
-        FeedPrice,
-    }
-    enum QueryMsg {
-        GlobalTime,
-    }
-
-    fn execute(
-        deps: DepsMut,
-        env: Env,
-        _info: MessageInfo,
-        _msg: ExecuteMsg,
-    ) -> StdResult<Response> {
-        update_global_time(deps.storage, &env)
-    }
-
-    fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
-        match msg {
-            QueryMsg::GlobalTime => to_binary(&query_global_time(deps)?),
-        }
-    }
+    use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env};
+    use cosmwasm_std::{coins};
 
     #[test]
     fn test_update_and_query_global_time() {
         let mut deps = mock_dependencies_with_balance(&coins(10, "unolus"));
-        let info = mock_info("feeder", &coins(10, "unolus"));
-        let msg = ExecuteMsg::FeedPrice;
         let env = mock_env();
 
-        execute(deps.as_mut(), env.clone(), info, msg).expect("can't update global time");
+        update_global_time(&mut deps.storage, &env).expect("can't update global time");
 
-        let msg = QueryMsg::GlobalTime;
-
-        let res = query(deps.as_ref(), mock_env(), msg).expect("can't query global time");
-        let time_response: GlobalTimeResponse =
-            from_binary(&res).expect("can't deserialize GlobalTimeResponse");
+        let time_response = query_global_time(&deps.storage).expect("can't query global time");
 
         assert_eq!(env.block.time, time_response.time);
     }
