193a90a348942ac1a30cb5b92792360cf0adf297
Desislava <desislava2810@abv.bg>
2022-04-06 11:35:09 +0300

    feat: Add deploy-contracts script

---
+++ scripts/deploy-contracts-dev-net.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -euxo pipefail
+
+ARTIFACT_BIN="nolus.tar.gz"
+NOLUS_DEV_NET="https://net-dev.nolus.io:26612"
+ACCOUNTS_DIR="$(pwd)/accounts"
+export TXFLAG="--gas-prices 0.025unolus --gas auto --gas-adjustment 1.3 -y --home $ACCOUNTS_DIR --node $NOLUS_DEV_NET"
+
+deployContract() {
+    RUSTFLAGS='-C link-arg=-s' cargo wasm --target-dir ./contracts/$1/target
+    docker run --rm -v "$(pwd)":/code --mount type=volume,source="devcontract_cache_$1",target=/code/target --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry cosmwasm/rust-optimizer:0.12.5 ./contracts/$1
+
+    RES=$(nolusd tx wasm store artifacts/$1.wasm --from treasury ${TXFLAG} --output json -b block)
+    CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[0].value')
+
+    nolusd tx wasm instantiate $CODE_ID "$2" --from treasury --label "$1" ${TXFLAG} --no-admin
+    sleep 6
+    CONTRACT_ADDRESS=$(nolusd query wasm list-contract-by-code $CODE_ID --node $NOLUS_DEV_NET --output json | jq -r '.contracts[-1]')
+
+    if [[ ! -e "contracts-addresses" ]]; then
+        mkdir "contracts-addresses"
+    fi
+
+PAIR=$(cat <<-EOF
+CODE_ID=${CODE_ID}
+CONTRACT_ADDRESS=${CONTRACT_ADDRESS}
+EOF
+)
+echo "$PAIR" > "contracts-addresses/$1"
+}
+
+
+if [[ $# -eq 0 ]]; then
+ if [[ -z ${CI_JOB_TOKEN+x} ]]; then
+    echo "Error: there is no PRIVATE or CI_JOB token"
+    exit 1
+  else
+    TOKEN_TYPE="JOB-TOKEN"
+    TOKEN_VALUE="$CI_JOB_TOKEN"
+  fi
+else
+  TOKEN_TYPE="PRIVATE-TOKEN"
+  TOKEN_VALUE="$1"
+fi
+
+VERSION=$(curl --silent "$NOLUS_DEV_NET/abci_info" | jq '.result.response.version' | tr -d '"')
+
+curl --output binary.zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "https://gitlab-nomo.credissimo.net/api/v4/projects/3/jobs/artifacts/v$VERSION/download?job=build-binary"
+echo 'A' | unzip binary.zip
+tar -xf $ARTIFACT_BIN
+export PATH=$(pwd):$PATH
+
+curl --output artifacts.zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "https://gitlab-nomo.credissimo.net/api/v4/projects/3/jobs/artifacts/v$VERSION/download?job=setup-dev-network"
+echo 'A' | unzip artifacts.zip
+
+# deploy all contracts
+INIT_MSG_ORACLE='{"base_asset":"ust","price_feed_period":20,"feeders_percentage_needed":50}'
+deployContract "oracle" $INIT_MSG_ORACLE
+
