0d8f6f1630e55e09e3d205854f9b3ea9d812d448
Desislava <desislava2810@abv.bg>
2022-04-13 13:58:38 +0300

    feat: Prepare the script for migration

--- scripts/deploy-contracts-dev-net.sh
+++ scripts/deploy-contracts-dev-net.sh
@@ -2,20 +2,31 @@
 set -euxo pipefail
 
 ROOT_DIR=$(pwd)
-ARTIFACT_BIN="nolus.tar.gz"
+BINARY_ARTIFACT_BIN="nolus.tar.gz"
+CONTRACTS_ARTIFACT_BIN="contracts.tar.gz"
+
 NOLUS_DEV_NET="https://net-dev.nolus.io:26612"
 ACCOUNTS_DIR="$(pwd)/accounts"
 export TXFLAG="--gas-prices 0.025unolus --gas auto --gas-adjustment 1.3 -y --home $ACCOUNTS_DIR --node $NOLUS_DEV_NET"
+source msgs.env #load init msgs of contracts as env variables
 
 deployContract() {
 
     RES=$(nolusd tx wasm store artifacts/$1.wasm --from treasury ${TXFLAG} --output json -b block)
-    CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[0].value')
+    NEW_CODE_ID=$(echo $RES | jq -r '.logs[0].events[-1].attributes[0].value')
 
-    #nolusd tx wasm instantiate $CODE_ID "$2" --from treasury --label "$1" ${TXFLAG} --no-admin
-    #  sleep 6
-    #CONTRACT_ADDRESS=$(nolusd query wasm list-contract-by-code $CODE_ID --node $NOLUS_DEV_NET --output json | jq -r '.contracts[-1]')
+    # if there is no $1 dir in the latest version of deploy-contracts artifact -> this is a new contract, so we instantiate it
+    if [[ ! -e "xxxxxlast-contracts-version/contracts-results/$1" ]]; then
+        nolusd tx wasm instantiate $NEW_CODE_ID "$2" --from treasury --label "$1" ${TXFLAG} --no-admin
+        sleep 6
+        CONTRACT_ADDRESS=$(nolusd query wasm list-contract-by-code $NEW_CODE_ID --node $NOLUS_DEV_NET --output json | jq -r '.contracts[-1]')
+    else # else this is an existing contract, so we migrate it
+        source last-contracts-version/contracts-addresses/$1/info.env
+        echo "migr"
+        # nolusd tx wasm migrate ${CONTRACT_ADDRESS} $NEW_CODE_ID [json_encoded_migration_args] --from treasury
+    fi
 
+    # prepare the results in contracts-results dir to be saved as artifact
     if [[ ! -e "contracts-results" ]]; then
         mkdir "contracts-results"
     fi
@@ -24,24 +35,23 @@ deployContract() {
         rm -rf contracts-results/$1
     fi
 
+    # generate schema
     cd contracts/$1
     cargo schema
 
     mkdir $ROOT_DIR/contracts-results/$1
     cp -R schema $ROOT_DIR/contracts-results/$1
 
-
-#CONTRACT_ADDRESS=${CONTRACT_ADDRESS}
 INFO=$(cat <<-EOF
-CODE_ID=${CODE_ID}
+export CONTRACT_ADDRESS=${CONTRACT_ADDRESS}
+export CODE_ID=${NEW_CODE_ID}
 EOF
 )
-echo "$INFO" > "$ROOT_DIR/contracts-results/$1/info"
-
+echo "$INFO" > "$ROOT_DIR/contracts-results/$1/info.env"
 cd $ROOT_DIR
 }
 
-
+# Download the build-binary and setup-dev-network artifacts from cosmozone
 if [[ $# -eq 0 ]]; then
  if [[ -z ${CI_JOB_TOKEN+x} ]]; then
     echo "Error: there is no PRIVATE or CI_JOB token"
@@ -59,17 +69,28 @@ VERSION=$(curl --silent "$NOLUS_DEV_NET/abci_info" | jq '.result.response.versio
 
 curl --output binary.zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "https://gitlab-nomo.credissimo.net/api/v4/projects/3/jobs/artifacts/v$VERSION/download?job=build-binary"
 echo 'A' | unzip binary.zip
-tar -xf $ARTIFACT_BIN
+tar -xf $BINARY_ARTIFACT_BIN
 export PATH=$(pwd):$PATH
 
 curl --output artifacts.zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "https://gitlab-nomo.credissimo.net/api/v4/projects/3/jobs/artifacts/v$VERSION/download?job=setup-dev-network"
 echo 'A' | unzip artifacts.zip
 
-# deploy all contracts
+# Deploy or migrate contracts
+CONTRACTS_VERSION=$(curl --header "$TOKEN_TYPE: $TOKEN_VALUE" "https://gitlab-nomo.credissimo.net/api/v4/projects/8/repository/tags" | jq '.[0].name' | tr -d '"')
 
-# if we need CONTRACT_ADDRESS var we will send init msg to deployContract():
-#INIT_MSG_ORACLE='{"base_asset":"ust","price_feed_period":60,"feeders_percentage_needed":50}'
-deployContract "oracle"
-deployContract "loan"
-deployContract "treasury"
-deployContract "borrow"
+  if [[ -d "last-contracts-version" ]]; then
+      rm -rf last-contracts-version
+  fi
+
+mkdir last-contracts-version
+cd last-contracts-version
+curl --output contracts.zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "https://gitlab-nomo.credissimo.net/api/v4/projects/8/jobs/artifacts/$CONTRACTS_VERSION/download?job=deploy:cargo"
+echo 'A' | unzip contracts.zip
+tar -xf $CONTRACTS_ARTIFACT_BIN
+cd $ROOT_DIR
+
+
+deployContract "oracle" ${ORACLE_MSG}
+deployContract "borrow" ${BORROW_MSG}
+deployContract "loan" ${LOAN_MSG}
+# deployContract "treasury" ${TREASURY_MSG}
