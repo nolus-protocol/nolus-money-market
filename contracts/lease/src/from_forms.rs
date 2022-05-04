a3ad3b2a87471acb82b1cd1e88ae7631c0b407fb
Gancho Manev <g.manev@nolus.io>
2022-05-04 14:13:30 +0300

    intro lpp and lpp stub

--- contracts/lease/src/from_forms.rs
+++ contracts/lease/src/from_forms.rs
@@ -5,6 +5,8 @@ use crate::{opening::{LoanForm, NewLeaseForm}, loan::Loan, lease::Lease};
 impl LoanForm {
     pub fn into(self, api: &dyn Api) -> StdResult<Loan> {
         let lpp = api.addr_validate(&self.lpp)?;
+        let _lpp_stub = lpp::stub::LppStub::from(lpp.clone());
+        // lpp_stub.create_open_loan_msg();
         Ok(Loan::new(
             self.annual_margin_interest_permille,
             lpp,
---
+++ contracts/lpp/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod msg;
+pub mod stub;
+
+#[cfg(not(feature = "library"))]
+#[path=""]
+mod nolib {
+    pub mod contract;
+    pub mod error;
+    pub mod state;
+    pub mod config;
+    pub mod loan;
+}
+
+#[cfg(not(feature = "library"))]
+pub use nolib::*;
