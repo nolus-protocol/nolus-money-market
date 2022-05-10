24b3faa12223ded7d796385beb3897ae9ae330b7
Nina Nikolova <n.nikolova@nolus.io>
2022-05-10 18:05:47 +0300

    fix cosmwasm optimization

--- contracts/lpp/src/lib.rs
+++ contracts/lpp/src/lib.rs
@@ -1,15 +1,17 @@
-pub mod contract;
 pub mod error;
 pub mod msg;
 pub mod stub;
 
-#[cfg(not(feature = "library"))]
-#[path = ""]
-mod nolib {
-    pub mod config;
-    pub mod loan;
-    pub mod state;
-}
+#[cfg(feature = "cosmwasm")]
+pub mod contract;
 
-#[cfg(not(feature = "library"))]
-pub use nolib::*;
+// #[cfg(not(feature = "library"))]
+// #[path = ""]
+// mod nolib {
+//     pub mod config;
+//     pub mod loan;
+//     pub mod state;
+// }
+
+// #[cfg(not(feature = "library"))]
+// pub use nolib::*;
