25bd72a18a09bc2553bf1a6f466b56f050d4b8c3
Gancho Manev <g.manev@nolus.io>
2022-05-12 21:10:38 +0300

    cleanup

--- contracts/lease/src/lib.rs
+++ contracts/lease/src/lib.rs
@@ -2,12 +2,14 @@ pub mod msg;
 pub mod opening;
 pub mod liability;
 pub mod error;
+mod amount;
+mod percent;
 
 #[cfg(feature = "cosmwasm")]
 pub mod contract;
-
+#[cfg(feature = "cosmwasm")]
 mod lease;
+#[cfg(feature = "cosmwasm")]
 mod loan;
+#[cfg(feature = "cosmwasm")]
 mod from_forms;
-mod amount;
-mod percent;
