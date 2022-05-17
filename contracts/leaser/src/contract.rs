142f1e772dc51997c4b3fbd6f14f71914eb2ecc3
Gancho Manev <g.manev@nolus.io>
2022-05-17 10:03:55 +0300

    wasm entrypoints are now visible

--- contracts/leaser/src/contract.rs
+++ contracts/leaser/src/contract.rs
@@ -1,4 +1,4 @@
-#[cfg(feature = "cosmwasm_bindings")]
+#[cfg(feature = "cosmwasm-bindings")]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
     to_binary, Addr, Binary, Coin, CosmosMsg, Decimal, Deps, DepsMut, Env, MessageInfo, Reply,
@@ -16,7 +16,7 @@ use crate::state::LS;
 const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[cfg_attr(feature = "cosmwasm_bindings", entry_point)]
+#[cfg_attr(feature = "cosmwasm-bindings", entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
@@ -29,7 +29,7 @@ pub fn instantiate(
     Ok(Response::default())
 }
 
-#[cfg_attr(feature = "cosmwasm_bindings", entry_point)]
+#[cfg_attr(feature = "cosmwasm-bindings", entry_point)]
 pub fn execute(
     deps: DepsMut,
     _env: Env,
@@ -46,7 +46,7 @@ pub fn execute(
     }
 }
 
-#[cfg_attr(feature = "cosmwasm_bindings", entry_point)]
+#[cfg_attr(feature = "cosmwasm-bindings", entry_point)]
 pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::Config {} => to_binary(&query_config(deps)?),
@@ -54,7 +54,7 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     }
 }
 
-#[cfg_attr(feature = "cosmwasm_bindings", entry_point)]
+#[cfg_attr(feature = "cosmwasm-bindings", entry_point)]
 pub fn reply(deps: DepsMut, _env: Env, msg: Reply) -> Result<Response, ContractError> {
     let contract_addr_raw = parse_reply_instantiate_data(msg.clone())
         .map(|r| r.contract_address)
