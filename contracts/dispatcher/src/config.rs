687846df61bc8754d8bdc885ae9a35c5a17ad4f9
Nina Nikolova <n.nikolova@nolus.io>
2022-05-22 12:34:54 +0300

    rewards dispatcher initial commit

---
+++ contracts/dispatcher/src/config.rs
@@ -0,0 +1,45 @@
+use cosmwasm_std::{Addr, StdResult, Storage};
+use cw_storage_plus::Item;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use crate::ContractError;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub cadence_hours: u32,
+    pub owner: Addr,
+    pub treasury: Addr,
+    pub time_oracle: Addr,
+}
+
+impl Config {
+    const STORAGE: Item<'static, Self> = Item::new("dispatcher_config");
+
+    pub fn new(owner: Addr, cadence_hours: u32, treasury: Addr, time_oracle: Addr) -> Self {
+        Config {
+            cadence_hours,
+            owner,
+            treasury,
+            time_oracle,
+        }
+    }
+
+    pub fn store(self, storage: &mut dyn Storage) -> StdResult<()> {
+        Self::STORAGE.save(storage, &self)
+    }
+
+    pub fn load(storage: &dyn Storage) -> StdResult<Self> {
+        Self::STORAGE.load(storage)
+    }
+
+    pub fn update(storage: &mut dyn Storage, cadence_hours: u32) -> Result<(), ContractError> {
+        Self::load(storage)?;
+        Self::STORAGE.update(storage, |mut c| -> Result<Config, ContractError> {
+            c.cadence_hours = cadence_hours;
+
+            Ok(c)
+        })?;
+        Ok(())
+    }
+}
---
+++ contracts/dispatcher/src/error.rs
@@ -0,0 +1,25 @@
+use cosmwasm_std::{Addr, StdError};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+    #[error("Custom Error val: {val:?}")]
+    CustomError { val: String },
+
+    // Add any other custom errors you like here.
+    // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
+    #[error("Invalid contract address {0}")]
+    InvalidContractAddress(Addr),
+
+    #[error("Invalid alarm notification address: {0:?}")]
+    InvalidAlarmAddress(Addr),
+
+    #[error("Alarm comming from unknown address: {0:?}")]
+    UnrecognisedAlarm(Addr),
+}
