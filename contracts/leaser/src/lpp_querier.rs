6cc250415fd11530ca4bc24dc4621aa76ce16b33
Nina Nikolova <n.nikolova@nolus.io>
2022-06-02 14:47:29 +0300

    use query_wasm_smart

--- contracts/leaser/src/lpp_querier.rs
+++ contracts/leaser/src/lpp_querier.rs
@@ -4,7 +4,7 @@ pub struct LppQuerier {}
 impl LppQuerier {
     #[cfg(not(test))]
     pub fn get_annual_interest_rate(deps: Deps, downpayment: Coin) -> StdResult<Decimal> {
-        use cosmwasm_std::{to_binary, QueryRequest, StdError, WasmQuery};
+        use cosmwasm_std::StdError;
         use lpp::msg::{QueryMsg as LppQueryMsg, QueryQuoteResponse};
 
         use crate::state::config::Config;
@@ -13,11 +13,9 @@ impl LppQuerier {
         let query_msg: LppQueryMsg = LppQueryMsg::Quote {
             amount: downpayment,
         };
-        let query_response: QueryQuoteResponse =
-            deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
-                contract_addr: config.lpp_ust_addr.to_string(),
-                msg: to_binary(&query_msg)?,
-            }))?;
+        let query_response: QueryQuoteResponse = deps
+            .querier
+            .query_wasm_smart(config.lpp_ust_addr.to_string(), &query_msg)?;
         match query_response {
             QueryQuoteResponse::QuoteInterestRate(rate) => Ok(rate),
             QueryQuoteResponse::NoLiquidity => Err(StdError::generic_err("NoLiquidity")),
