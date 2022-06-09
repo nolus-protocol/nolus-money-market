0b97b4488d220471ed88e1e863985870638cb165
Desislava <d.paunova@nolus.io>
2022-06-09 15:03:17 +0300

    refactor

--- scripts/common/deploy-contracts.sh
+++ scripts/common/deploy-contracts.sh
@@ -1,57 +1,59 @@
 #!/bin/bash
 set -euxo pipefail
-
-source "$COMMON_DIR/cmd.sh"
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+source "$SCRIPT_DIR"/cmd.sh
 
 deployContract() {
-  local contract_name="$1"
-  local txflags="--gas-prices 0.025unolus --gas auto --gas-adjustment 1.3 -y --node $NOLUS_NET"
+  local -r contracts_results_file="$1"
+  local -r nolus_net="$2"
+  local -r home_dir="$3"
+  local -r contract_name="$4"
+  local -r txflags="--gas-prices 0.025unolus --gas auto --gas-adjustment 1.3 -y --node $nolus_net"
 
- RES=$(run_cmd "$HOME_DIR" tx wasm store artifacts/"$contract_name".wasm --from treasury $txflags --output json -b "block")
- FIX_RESPONSE='{"height"'${RES#*'{"height"'}
- CODE_ID=$(echo "$FIX_RESPONSE" | jq -r '.logs[0].events[-1].attributes[0].value')
+ local -r res=$(run_cmd "$home_dir" tx wasm store artifacts/"$contract_name".wasm --from treasury $txflags --output json -b "block")
+ local -r fix_response='{"height"'${res#*'{"height"'}
+ local -r code_id=$(echo "$fix_response" | jq -r '.logs[0].events[-1].attributes[0].value')
 
-  if [[ $# -eq 1 ]]; then
-    local info='{"'$contract_name'":{"code_id":"'$CODE_ID'"}}'
+  if [[ $# -eq 4 ]]; then
+    local -r info='{"'$contract_name'":{"code_id":"'$code_id'"}}'
   else
-    local init_msg="$2"
-    run_cmd "$HOME_DIR" tx wasm instantiate "$CODE_ID" "$init_msg" --from treasury --label "$contract_name" $txflags --no-admin -b "block"
-    CONTRACT_ADDRESS=$(run_cmd "$HOME_DIR" query wasm list-contract-by-code "$CODE_ID" --node "$NOLUS_NET" --output json | jq -r '.contracts[-1]')
-    local info='{"'$contract_name'":{"instance":"'$CONTRACT_ADDRESS'","code_id":"'$CODE_ID'"}}'
+    local -r init_msg="$5"
+    run_cmd "$home_dir" tx wasm instantiate "$code_id" "$init_msg" --from treasury --label "$contract_name" $txflags --no-admin -b "block"
+    local -r contract_address=$(run_cmd "$home_dir" query wasm list-contract-by-code "$code_id" --node "$nolus_net" --output json | jq -r '.contracts[-1]')
+    local -r info='{"'$contract_name'":{"instance":"'$contract_address'","code_id":"'$code_id'"}}'
   fi
-  jq --argjson contract_info "$info" '.contracts_info |= . + [$contract_info]' "$CONTRACTS_RESULTS_FILE" > tmp.json && mv tmp.json "$CONTRACTS_RESULTS_FILE"
+  jq --argjson contract_info "$info" '.contracts_info |= . + [$contract_info]' "$contracts_results_file" > tmp.json && mv tmp.json "$contracts_results_file"
 }
 
 deployContracts() {
-CONTRACTS_RESULTS_FILE="$1"
-NOLUS_NET="$2"
-HOME_DIR="$3"
-STABLE_DENOM="$4"
+local -r contracts_results_file="$1"
+local -r nolus_net="$2"
+local -r home_dir="$3"
+local -r stable_denom="$4"
 
-jq -n '{"contracts_info":[]}' > "$CONTRACTS_RESULTS_FILE"
+jq -n '{"contracts_info":[]}' > "$contracts_results_file"
 
-deployContract "lease"
-LEASE_CODE_ID=$(jq .contracts_info[0].lease.code_id contracts-info.json | tr -d '"')
+deployContract "$contracts_results_file" "$nolus_net" "$home_dir" "lease"
+local -r lease_code_id=$(jq .contracts_info[0].lease.code_id contracts-info.json | tr -d '"')
 
-LPP_INIT_MSG='{"denom":"'$STABLE_DENOM'","lease_code_id":"'$LEASE_CODE_ID'"}'
-deployContract "lpp" "$LPP_INIT_MSG"
-LPP_ADDRESS=$(jq .contracts_info[1].lpp.instance contracts-info.json | tr -d '"')
+local -r lpp_init_msg='{"denom":"'$stable_denom'","lease_code_id":"'$lease_code_id'"}'
+deployContract "$contracts_results_file" "$nolus_net" "$home_dir" "lpp"  "$lpp_init_msg"
+local -r lpp_address=$(jq .contracts_info[1].lpp.instance contracts-info.json | tr -d '"')
 
-LEASER_INIT_MSG='{"lease_code_id":"'$LEASE_CODE_ID'","lease_interest_rate_margin":30,"recalc_hours":2,"liability":{"healthy":70,"initial":65,"max":80},"lpp_ust_addr":"'$LPP_ADDRESS'","repayment":{"grace_period_sec":864000,"period_sec":5184000}}'
-deployContract "leaser" "$LEASER_INIT_MSG"
+local -r leaser_init_msg='{"lease_code_id":"'$lease_code_id'","lease_interest_rate_margin":30,"recalc_hours":2,"liability":{"healthy":70,"initial":65,"max":80},"lpp_ust_addr":"'$lpp_address'","repayment":{"grace_period_sec":864000,"period_sec":5184000}}'
+deployContract "$contracts_results_file" "$nolus_net" "$home_dir" "leaser" "$leaser_init_msg"
 
-ORACLE_INIT_MSG='{"base_asset":"'$STABLE_DENOM'","price_feed_period":60,"feeders_percentage_needed":50,"supported_denom_pairs":[["OSMO","'$STABLE_DENOM'"],["LUNA","OSMO"],["IRIS","OSMO"]]}'
-deployContract "oracle" "$ORACLE_INIT_MSG"
-ORACLE_ADDRESS=$(jq .contracts_info[3].oracle.instance contracts-info.json | tr -d '"')
+local -r oracle_init_msg='{"base_asset":"'$stable_denom'","price_feed_period":60,"feeders_percentage_needed":50,"supported_denom_pairs":[["OSMO","'$stable_denom'"],["LUNA","OSMO"],["IRIS","OSMO"]]}'
+deployContract "$contracts_results_file" "$nolus_net" "$home_dir" "oracle" "$oracle_init_msg"
+local -r oracle_address=$(jq .contracts_info[3].oracle.instance contracts-info.json | tr -d '"')
 
-TREASURY_INIT_MSG='{}'
-deployContract "treasury" "$TREASURY_INIT_MSG"
-TREASURY_ADDRESS=$(jq .contracts_info[4].treasury.instance contracts-info.json | tr -d '"')
+local -r treasury_init_msg='{}'
+deployContract "$contracts_results_file" "$nolus_net" "$home_dir" "treasury" "$treasury_init_msg"
+local -r treasury_address=$(jq .contracts_info[4].treasury.instance contracts-info.json | tr -d '"')
 
+local -r profit_init_msg='{"cadence_hours":7200,"treasury":"'$treasury_address'","time_oracle":"'$oracle_address'"}'
+deployContract "$contracts_results_file" "$nolus_net" "$home_dir" "profit" "$profit_init_msg"
 
-PROFIT_INIT_MSG='{"cadence_hours":7200,"treasury":"'$TREASURY_ADDRESS'","time_oracle":"'$ORACLE_ADDRESS'"}'
-deployContract "profit" "$PROFIT_INIT_MSG"
-
-DISPATCHER_INIT_MSG='{"cadence_hours":7200,"lpp":"'$LPP_ADDRESS'","time_oracle":"'$ORACLE_ADDRESS'","treasury":"'$TREASURY_ADDRESS'","market_oracle":"'$ORACLE_ADDRESS'","tvl_to_apr":{"intervals":[{"tvl":0,"apr":300},{"tvl":1000,"apr":90},{"tvl":1000000,"apr":30}]}}';
-deployContract "rewards_dispatcher" "$DISPATCHER_INIT_MSG"
+local -r dispatcher_init_msg='{"cadence_hours":7200,"lpp":"'$lpp_address'","time_oracle":"'$oracle_address'","treasury":"'$treasury_address'","market_oracle":"'$oracle_address'","tvl_to_apr":{"intervals":[{"tvl":0,"apr":300},{"tvl":1000,"apr":90},{"tvl":1000000,"apr":30}]}}';
+deployContract "$contracts_results_file" "$nolus_net" "$home_dir" "rewards_dispatcher" "$dispatcher_init_msg"
 }
