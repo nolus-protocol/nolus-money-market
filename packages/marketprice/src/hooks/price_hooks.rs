1c126a2d4ca17be65870fb64bcf7869f32251730
Nina Nikolova <n.nikolova@nolus.io>
2022-06-17 17:55:37 +0300

    collect rules responces

--- packages/marketprice/src/hooks/price_hooks.rs
+++ packages/marketprice/src/hooks/price_hooks.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Addr, CosmosMsg, DepsMut, Response, StdResult, Storage, SubMsg};
+use cosmwasm_std::{Addr, CosmosMsg, DepsMut, Order, Response, StdResult, Storage, SubMsg};
 use cw_storage_plus::Map;
 
 use super::{errors::HooksError, msg::ExecuteHookMsg, SimpleRule};
@@ -44,15 +44,21 @@ impl<'m> PriceHooks<'m> {
         Ok(Response::new().add_attribute("method", "try_remove_all"))
     }
 
-    pub fn check_rules(&self, storage: &mut dyn Storage) {
-        // let notifications: Vec<SubMsg> = self
-        //     .0
-        //     .prefix(())
-        //     .range(storage, None, None, Order::Ascending)
-        //     .map(|item| Self::evaluate(item.unwrap()))
-        //     .collect::<Vec<SubMsg>>();
+    pub fn check_rules(&self, storage: &mut dyn Storage) -> StdResult<Vec<SubMsg>> {
+        let notifications: Vec<_> = self
+            .0
+            .prefix(())
+            .range(storage, None, None, Order::Ascending)
+            .map(|item| Self::evaluate(item.unwrap()).unwrap())
+            .collect();
 
-        // self.response.messages.push(submsg);
+        let size = notifications.iter().fold(0, |a, b| a + b.len());
+        Ok(notifications
+            .into_iter()
+            .fold(Vec::with_capacity(size), |mut acc, v| {
+                acc.extend(v);
+                acc
+            }))
     }
 
     fn evaluate((addr, rules): (Addr, Vec<SimpleRule>)) -> StdResult<Vec<SubMsg>> {
