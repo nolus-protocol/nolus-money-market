d16473c34eb8ed710ae87eddd3380f1c642aeff3
v.kolesnikov <v.kolesnikov@nolus.io>
2022-07-27 19:52:10 +1000

    [lpp] extend schema for responses

--- contracts/lpp/examples/schema.rs
+++ contracts/lpp/examples/schema.rs
@@ -2,8 +2,24 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use finance::currency::Nls;
 
-use lpp::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use lpp::msg::{
+    ExecuteMsg, 
+    InstantiateMsg, 
+    QueryMsg,
+
+    QueryConfigResponse,
+    QueryQuoteResponse,
+    LoanResponse,
+    QueryLoanResponse,
+    QueryLoanOutstandingInterestResponse,
+    BalanceResponse,
+    PriceResponse,
+    LppBalanceResponse,
+    RewardsResponse,
+};
+
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -14,4 +30,14 @@ fn main() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
+
+    export_schema(&schema_for!(QueryConfigResponse), &out_dir);
+    export_schema(&schema_for!(QueryQuoteResponse), &out_dir);
+    export_schema(&schema_for!(LoanResponse<Nls>), &out_dir);
+    export_schema(&schema_for!(QueryLoanResponse<Nls>), &out_dir);
+    export_schema(&schema_for!(QueryLoanOutstandingInterestResponse<Nls>), &out_dir);
+    export_schema(&schema_for!(BalanceResponse), &out_dir);
+    export_schema(&schema_for!(PriceResponse<Nls>), &out_dir);
+    export_schema(&schema_for!(LppBalanceResponse<Nls>), &out_dir);
+    export_schema(&schema_for!(RewardsResponse), &out_dir);
 }
