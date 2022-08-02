8002472e8945268b876997fb6e8a3bb3904bec75
Kiril Mihaylov <k.mihaylov@nolus.io>
2022-08-02 17:12:28 +0300

    test: Added unit testing

--- contracts/lease/src/loan/repay.rs
+++ contracts/lease/src/loan/repay.rs
@@ -14,7 +14,7 @@ where
     pub paid: LoanInterestsPaid<C>,
 }
 
-#[derive(Default)]
+#[derive(Default, Clone, Eq, PartialEq)]
 pub(crate) struct LoanInterestsPaid<C>
 where
     C: Currency,
@@ -79,5 +79,154 @@ where
         self.principal_paid = payment;
 
         self.close = principal == payment;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use finance::{
+        coin::Coin,
+        currency::Nls
+    };
+
+    use crate::loan::LoanInterestsPaid;
+
+    fn pay_margin_once() {
+        let amount = Coin::<Nls>::new(5);
+
+        let mut paid = LoanInterestsPaid::default();
+
+        paid.pay_next_margin(amount);
+
+        assert_eq!(
+            paid,
+            LoanInterestsPaid {
+                current_margin_paid: amount,
+                .. Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn test_pay_margin_once() {
+        pay_margin_once();
+    }
+
+    fn pay_margin_twice() {
+        let amount_1 = Coin::<Nls>::new(5);
+
+        let amount_2 = Coin::<Nls>::new(15);
+
+        let mut paid = LoanInterestsPaid::default();
+
+        paid.pay_next_margin(amount_1);
+
+        paid.pay_next_margin(amount_2);
+
+        assert_eq!(
+            paid,
+            LoanInterestsPaid {
+                previous_margin_paid: amount_1,
+                current_margin_paid: amount_2,
+                .. Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn test_pay_margin_twice() {
+        pay_margin_twice();
+    }
+
+    fn pay_interest_once() {
+        let amount = Coin::<Nls>::new(5);
+
+        let mut paid = LoanInterestsPaid::default();
+
+        paid.pay_next_interest(amount);
+
+        assert_eq!(
+            paid,
+            LoanInterestsPaid {
+                current_interest_paid: amount,
+                .. Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn test_pay_interest_once() {
+        pay_interest_once();
+    }
+
+    fn pay_interest_twice() {
+        let amount_1 = Coin::<Nls>::new(5);
+
+        let amount_2 = Coin::<Nls>::new(15);
+
+        let mut paid = LoanInterestsPaid::default();
+
+        paid.pay_next_interest(amount_1);
+
+        paid.pay_next_interest(amount_2);
+
+        assert_eq!(
+            paid,
+            LoanInterestsPaid {
+                previous_interest_paid: amount_1,
+                current_interest_paid: amount_2,
+                .. Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn test_pay_interest_twice() {
+        pay_interest_twice();
+    }
+
+    fn pay_principal_part() {
+        let principal = Coin::<Nls>::new(10);
+
+        let amount = Coin::<Nls>::new(5);
+
+        let mut paid = LoanInterestsPaid::default();
+
+        paid.pay_principal(principal, amount);
+
+        assert_eq!(
+            paid,
+            LoanInterestsPaid {
+                principal_paid: amount,
+                .. Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn test_pay_principal_part() {
+        pay_principal_part();
+    }
+
+    fn pay_principal_full() {
+        let principal = Coin::<Nls>::new(10);
+
+        let mut paid = LoanInterestsPaid::default();
+
+        paid.pay_principal(principal, principal);
+
+        assert_eq!(
+            paid,
+            LoanInterestsPaid {
+                principal_paid: principal,
+                close: true,
+                .. Default::default()
+            },
+        );
+    }
+
+    #[test]
+    fn test_pay_principal_full() {
+        pay_principal_full();
     }
 }
