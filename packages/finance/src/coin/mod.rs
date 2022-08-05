ce3631f6391880a85d97345b40cf66231c985e8f
Kiril Mihaylov <k.mihaylov@nolus.io>
2022-08-05 13:11:42 +0300

    feat: Implement SubAssign

--- packages/finance/src/coin/mod.rs
+++ packages/finance/src/coin/mod.rs
@@ -7,7 +7,7 @@ mod serde;
 use std::{
     fmt::{Debug, Display, Formatter, Write},
     marker::PhantomData,
-    ops::{Add, Div, Sub},
+    ops::{Add, Div, Sub, SubAssign},
 };
 
 use ::serde::{Deserialize, Serialize};
@@ -90,6 +90,15 @@ where
             amount: self.amount - rhs.amount,
             symbol: self.symbol,
         }
+    }
+}
+
+impl<C> SubAssign<Coin<C>> for Coin<C>
+where
+    C: Currency,
+{
+    fn sub_assign(&mut self, rhs: Coin<C>) {
+        self.amount -= rhs.amount;
     }
 }
 
