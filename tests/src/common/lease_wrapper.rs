e1b799043b15883d162933b1396c6e007cab82da
v.kolesnikov <v.kolesnikov@nolus.io>
2022-08-05 17:05:26 +1000

    [tests,lpp] extend integration test pkg, tests for deposit/withdraw

--- tests/src/common/lease_wrapper.rs
+++ tests/src/common/lease_wrapper.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{coins, Addr};
+use cosmwasm_std::{coin, Addr};
 use cw_multi_test::ContractWrapper;
 use finance::{liability::Liability, percent::Percent};
 use lease::msg::{LoanForm, NewLeaseForm};
@@ -27,6 +27,41 @@ pub struct LeaseWrapper {
     contract_wrapper: LeaseContractWrapperReply,
 }
 
+pub struct LeaseWrapperConfig {
+    //NewLeaseForm
+    pub customer: String,
+    // Liability
+    pub liability_init_percent: Percent,
+    pub liability_delta_to_healthy_percent: Percent,
+    pub liability_delta_to_max_percent: Percent,
+    pub liability_recalc_hours: u16,
+    // LoanForm
+    pub annual_margin_interest: Percent,
+    pub interest_due_period_secs: u32,
+    pub grace_period_secs: u32,
+
+    pub downpayment: u128,
+}
+
+impl Default for LeaseWrapperConfig {
+    fn default() -> Self {
+        Self {
+            customer: USER.to_string(),
+            liability_init_percent: Percent::from_percent(65),
+            liability_delta_to_healthy_percent: Percent::from_percent(5),
+            liability_delta_to_max_percent: Percent::from_percent(10),
+            liability_recalc_hours: 20 * 24,
+
+            annual_margin_interest: Percent::from_percent(0), // 3.1%
+            interest_due_period_secs: 100, // 90 days TODO use a crate for daytime calculations
+            grace_period_secs: 10,         // 10 days TODO use a crate for daytime calculations
+
+            // TODO: extend to Coin (downpayment and lpn can be different)
+            downpayment: 1000u128,
+        }
+    }
+}
+
 impl LeaseWrapper {
     pub fn store(self, app: &mut App) -> u64 {
         app.store_code(self.contract_wrapper)
@@ -39,39 +74,42 @@ impl LeaseWrapper {
         code_id: Option<u64>,
         lpp_addr: &Addr,
         denom: &str,
+        config: LeaseWrapperConfig,
     ) -> Addr {
         let code_id = match code_id {
             Some(id) => id,
             None => app.store_code(self.contract_wrapper),
         };
-        let msg = Self::lease_instantiate_msg(denom, lpp_addr.clone());
+
+        let downpayment = config.downpayment;
+        let msg = Self::lease_instantiate_msg(denom, lpp_addr.clone(), config);
 
         app.instantiate_contract(
             code_id,
             Addr::unchecked(ADMIN),
             &msg,
-            &coins(1000, denom),
+            &[coin(downpayment, denom)],
             "lease",
             None,
         )
         .unwrap()
     }
 
-    fn lease_instantiate_msg(denom: &str, lpp_addr: Addr) -> NewLeaseForm {
+    fn lease_instantiate_msg(denom: &str, lpp_addr: Addr, config: LeaseWrapperConfig) -> NewLeaseForm {
         NewLeaseForm {
-            customer: USER.to_string(),
+            customer: config.customer,
             currency: denom.to_string(),
             liability: Liability::new(
-                Percent::from_percent(65),
-                Percent::from_percent(5),
-                Percent::from_percent(10),
-                20 * 24,
+                config.liability_init_percent,
+                config.liability_delta_to_healthy_percent,
+                config.liability_delta_to_max_percent,
+                config.liability_recalc_hours,
             ),
             loan: LoanForm {
-                annual_margin_interest: Percent::from_percent(0), // 3.1%
+                annual_margin_interest: config.annual_margin_interest,
                 lpp: lpp_addr.into_string(),
-                interest_due_period_secs: 100, // 90 days TODO use a crate for daytime calculations
-                grace_period_secs: 10,         // 10 days TODO use a crate for daytime calculations
+                interest_due_period_secs: config.interest_due_period_secs,
+                grace_period_secs: config.grace_period_secs,
             },
         }
     }
