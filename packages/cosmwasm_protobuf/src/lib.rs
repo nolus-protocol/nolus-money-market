244ef63c00e4021f696afc408a150e137ca2104d
Kiril Mihaylov <k.mihaylov@nolus.io>
2022-08-23 14:18:33 +0300

    fix: Issues with serialization to protobuf format

--- packages/cosmwasm_protobuf/src/lib.rs
+++ packages/cosmwasm_protobuf/src/lib.rs
@@ -1,35 +1,36 @@
-use cosmwasm_std::{StdResult, from_binary, Reply, StdError, Addr};
-use serde::de::DeserializeOwned;
-
 //! For Protobuf layouts refer to [this source](https://github.com/CosmWasm/wasmd/blob/main/proto/cosmwasm/wasm/v1/tx.proto).
 //!
 //! Here are defined wrappers for deserializing such structures.
 
-pub fn from_instantiate_reply<T>(reply: Reply) -> StdResult<(Addr, T)>
+use cosmwasm_std::{StdResult, from_binary, Reply, StdError, Addr};
+use serde::de::DeserializeOwned;
+
+pub fn from_instantiate_reply<T>(reply: Reply) -> StdResult<(String, T)>
 where
     T: DeserializeOwned,
 {
     #[derive(prost::Message)]
     struct ReplyData {
         #[prost(bytes, tag = "1")]
-        pub address: Addr,
+        pub address: Vec<u8>,
         #[prost(bytes, tag = "2")]
         pub data: Vec<u8>,
     }
 
-    from_binary(
-        &<ReplyData as prost::Message>::decode(
-            reply.result.into_result()
-                .map_err(StdError::generic_err)?
-                .data
-                .ok_or_else(|| StdError::generic_err("Reply doesn't contain data!"))?
-                .0
-                .as_slice(),
-        )
-            .map_err(|_| StdError::generic_err("Data is malformed or doesn't comply with used protobuf format!"))?
+    let response = <ReplyData as prost::Message>::decode(
+        reply.result.into_result()
+            .map_err(StdError::generic_err)?
             .data
-            .into(),
-    )
+            .ok_or_else(|| StdError::generic_err("Reply doesn't contain data!"))?
+            .0
+            .as_slice(),
+    ).map_err(|_| StdError::generic_err("Data is malformed or doesn't comply with used protobuf format!"))?;
+
+    Ok((
+        String::from_utf8(response.address)
+            .map_err(|_| StdError::generic_err("Address field contains invalid UTF-8 data!"))?,
+        from_binary(&response.data.into())?,
+    ))
 }
 
 pub fn from_execute_reply<T>(reply: Reply) -> StdResult<T>
