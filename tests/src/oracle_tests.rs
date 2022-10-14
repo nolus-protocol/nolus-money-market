87e6f8d29887e77637a5152b8772991926a70bad
v.kolesnikov <v.kolesnikov@nolus.io>
2022-10-14 13:39:37 +1000

    [oracle] integration test for issue #18

--- tests/src/oracle_tests.rs
+++ tests/src/oracle_tests.rs
@@ -2,12 +2,14 @@ use std::collections::HashSet;
 
 use currency::lpn::Usdc;
 use finance::{
-    coin::Coin,
+    coin::{Amount, Coin},
     currency::Currency as _,
+    percent::Percent,
     price::{self, dto::PriceDTO},
     test::currency::Nls,
 };
 use leaser::msg::QueryMsg;
+use oracle::msg::QueryMsg as OracleQ;
 use platform::coin_legacy::to_cosmwasm;
 use sdk::{
     cosmwasm_std::{coins, wasm_execute, Addr, Event},
@@ -165,4 +167,105 @@ fn integration_with_timealarms() {
         .unwrap();
 
     resp.assert_event(&Event::new("wasm").add_attribute("alarm", "success"))
+}
+
+#[test]
+fn test_config_update() {
+    let mut test_case = create_test_case();
+
+    let admin = Addr::unchecked(ADMIN);
+    let feeder1 = Addr::unchecked("feeder1");
+    let feeder2 = Addr::unchecked("feeder2");
+    let feeder3 = Addr::unchecked("feeder3");
+    let base = 2;
+    let quote = 10;
+
+    fn add_feeder(test_case: &mut TestCase, addr: impl Into<String>) {
+        test_case
+            .app
+            .execute(
+                Addr::unchecked(ADMIN),
+                wasm_execute(
+                    test_case.oracle.clone().unwrap(),
+                    &oracle::msg::ExecuteMsg::RegisterFeeder {
+                        feeder_address: addr.into(),
+                    },
+                    vec![],
+                )
+                .unwrap()
+                .into(),
+            )
+            .unwrap();
+    }
+
+    fn add_price(test_case: &mut TestCase, addr: &Addr, base: Amount, quote: Amount) {
+        test_case
+            .app
+            .execute(
+                addr.clone(),
+                wasm_execute(
+                    test_case.oracle.clone().unwrap(),
+                    &oracle::msg::ExecuteMsg::FeedPrices {
+                        prices: vec![PriceDTO::try_from(
+                            price::total_of(Coin::<Nls>::new(base)).is(Coin::<Usdc>::new(quote)),
+                        )
+                        .unwrap()],
+                    },
+                    vec![],
+                )
+                .unwrap()
+                .into(),
+            )
+            .expect("Oracle not properly connected!");
+    }
+
+    add_feeder(&mut test_case, &feeder1);
+    add_feeder(&mut test_case, &feeder2);
+    add_feeder(&mut test_case, &feeder3);
+
+    add_price(&mut test_case, &feeder1, base, quote);
+    add_price(&mut test_case, &feeder2, base, quote);
+
+    let price: PriceDTO = test_case
+        .app
+        .wrap()
+        .query_wasm_smart(
+            test_case.oracle.clone().unwrap(),
+            &OracleQ::Price {
+                currency: Nls::SYMBOL.to_owned(),
+            },
+        )
+        .unwrap();
+
+    assert_eq!(
+        price,
+        PriceDTO::try_from(price::total_of(Coin::<Nls>::new(base)).is(Coin::<Usdc>::new(quote)),)
+            .unwrap()
+    );
+
+    test_case
+        .app
+        .execute(
+            admin,
+            wasm_execute(
+                test_case.oracle.clone().unwrap(),
+                &oracle::msg::ExecuteMsg::Config {
+                    price_feed_period_secs: 60,
+                    expected_feeders: Percent::from_percent(100),
+                },
+                vec![],
+            )
+            .unwrap()
+            .into(),
+        )
+        .expect("Oracle not properly connected!");
+
+    let price: Result<PriceDTO, _> = test_case.app.wrap().query_wasm_smart(
+        test_case.oracle.clone().unwrap(),
+        &OracleQ::Price {
+            currency: Nls::SYMBOL.to_owned(),
+        },
+    );
+
+    assert!(price.is_err());
 }
