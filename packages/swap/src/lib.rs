1bb4db2cd18bd1352894f384779e7415b3d5f990
Gancho Manev <g.manev@nolus.io>
2022-11-03 15:58:36 +0200

    [swap] complete the impl of swap exact amount in

--- packages/swap/src/lib.rs
+++ packages/swap/src/lib.rs
@@ -1,11 +1,12 @@
 use ::currency::payment::PaymentGroup;
 use finance::{
     coin::CoinDTO,
-    currency::{self, AnyVisitor, Currency, Group},
+    currency::{self, AnyVisitor, Currency, Symbol, SymbolOwned},
 };
-use oracle::msg::SwapPathResponse;
+use oracle::{msg::SwapPathResponse, state::supported_pairs::SwapTarget};
 use osmosis_std::types::{
-    cosmos::base::v1beta1::Coin, osmosis::gamm::v1beta1::MsgSwapExactAmountIn,
+    cosmos::base::v1beta1::Coin,
+    osmosis::gamm::v1beta1::{MsgSwapExactAmountIn, SwapAmountInRoute},
 };
 use platform::ica::Batch;
 use sdk::cosmwasm_std::Addr;
@@ -18,12 +19,18 @@ pub fn exact_amount_in(
     batch: &mut Batch,
     sender: &Addr,
     token_in: &CoinDTO,
-    _swap_path: &SwapPathResponse,
+    swap_path: &SwapPathResponse,
 ) -> Result<()> {
     const MSG_TYPE: &str = "/osmosis.gamm.v1beta1.MsgSwapExactAmountIn";
-    let routes = vec![];
-    let token_in = Some(into_coin::<SwapGroup>(token_in)?);
-    let token_out_min_amount = "0".into();
+    // TODO bring the token balances, weights and swapFee-s from the DEX pools
+    // into the oracle in order to calculate the tokenOut as per the formula at
+    // https://docs.osmosis.zone/osmosis-core/modules/gamm/#swap.
+    // Then apply the parameterized maximum slippage to get the minimum amount.
+    // For the first version, we accept whatever price impact and slippage.
+    const MIN_OUT_AMOUNT: &str = "0";
+    let routes = into_route(swap_path)?;
+    let token_in = Some(into_coin(token_in)?);
+    let token_out_min_amount = MIN_OUT_AMOUNT.into();
     let msg = MsgSwapExactAmountIn {
         sender: sender.into(),
         routes,
@@ -35,55 +42,96 @@ pub fn exact_amount_in(
     Ok(())
 }
 
-type SwapGroup = PaymentGroup;
+fn into_route(swap_path: &[SwapTarget]) -> Result<Vec<SwapAmountInRoute>> {
+    swap_path
+        .iter()
+        .map(|swap_target| {
+            to_dex_symbol(&swap_target.target).map(|dex_symbol| SwapTarget {
+                pool_id: swap_target.pool_id,
+                target: dex_symbol,
+            })
+        })
+        .map(|maybe_swap_target| {
+            maybe_swap_target.map(|swap_target| SwapAmountInRoute {
+                pool_id: swap_target.pool_id,
+                token_out_denom: swap_target.target,
+            })
+        })
+        .collect()
+}
 
-fn into_coin<G>(token: &CoinDTO) -> Result<Coin>
-where
-    G: Group,
-{
-    struct CoinFactory<'c> {
-        token: &'c CoinDTO,
-    }
-    impl<'c> AnyVisitor for CoinFactory<'c> {
-        type Output = Coin;
+fn into_coin(token: &CoinDTO) -> Result<Coin> {
+    Ok(Coin {
+        denom: to_dex_symbol(token.ticker())?,
+        amount: token.amount().to_string(),
+    })
+}
+
+fn to_dex_symbol(ticker: Symbol) -> Result<SymbolOwned> {
+    type SwapGroup = PaymentGroup;
+
+    struct ToDEXSymbol {}
+    impl AnyVisitor for ToDEXSymbol {
+        type Output = SymbolOwned;
         type Error = Error;
 
         fn on<C>(self) -> Result<Self::Output>
         where
             C: Currency,
         {
-            Ok(Self::Output {
-                denom: C::DEX_SYMBOL.into(),
-                amount: self.token.amount().to_string(),
-            })
+            Ok(C::DEX_SYMBOL.into())
         }
     }
-    currency::visit_any_on_ticker::<G, _>(token.ticker(), CoinFactory { token })
+    currency::visit_any_on_ticker::<SwapGroup, _>(ticker, ToDEXSymbol {})
 }
 
 #[cfg(test)]
 mod test {
     use currency::lpn::Usdc;
-    use finance::{
-        coin::{Amount, Coin as FinanceCoin, CoinDTO},
-        currency::Currency,
-    };
-    use osmosis_std::types::cosmos::base::v1beta1::Coin;
+    use finance::currency::{Currency, SymbolStatic};
+    use oracle::state::supported_pairs::SwapTarget;
+    use osmosis_std::types::osmosis::gamm::v1beta1::SwapAmountInRoute;
 
-    use crate::SwapGroup;
+    use crate::error::Error;
+
+    const INVALID_TICKER: SymbolStatic = "NotATicker";
 
     #[test]
-    fn into_coin() {
+    fn to_dex_symbol() {
         type Currency = Usdc;
-        const AMOUNT: Amount = 243;
-        let token = FinanceCoin::<Currency>::new(AMOUNT);
-        let token_dto: CoinDTO = token.into();
         assert_eq!(
-            Ok(Coin {
-                denom: Currency::DEX_SYMBOL.into(),
-                amount: AMOUNT.to_string()
-            }),
-            super::into_coin::<SwapGroup>(&token_dto)
+            Ok(Currency::DEX_SYMBOL.into()),
+            super::to_dex_symbol(Currency::TICKER)
         );
+    }
+
+    #[test]
+    fn to_dex_symbol_err() {
+        assert!(matches!(
+            super::to_dex_symbol(INVALID_TICKER),
+            Err(Error::Finance(_))
+        ));
+    }
+
+    #[test]
+    fn into_route() {
+        let path = vec![SwapTarget {
+            pool_id: 2,
+            target: Usdc::TICKER.into(),
+        }];
+        let expected = vec![SwapAmountInRoute {
+            pool_id: 2,
+            token_out_denom: Usdc::DEX_SYMBOL.into(),
+        }];
+        assert_eq!(Ok(expected), super::into_route(&path));
+    }
+
+    #[test]
+    fn into_route_err() {
+        let path = vec![SwapTarget {
+            pool_id: 2,
+            target: INVALID_TICKER.into(),
+        }];
+        assert!(matches!(super::into_route(&path), Err(Error::Finance(_))));
     }
 }
