ae5b91f1698e92e56015d79f3f71e4b82e7bee46
Kiril Mihaylov <k.mihaylov@nolus.io>
2022-12-22 11:16:07 +0200

    test: Added test for runtime panic on integer overflow

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,26 +1,20 @@
-#![cfg(not(target_arch = "wasm32"))]
+#![cfg(all(test, not(target_arch = "wasm32")))]
 
-#[cfg(test)]
 mod dispatcher_tests;
 
-#[cfg(test)]
 mod profit_tests;
 
-#[cfg(test)]
 mod lpp_tests;
 
-#[cfg(test)]
 #[allow(dead_code)]
 mod common;
 
-#[cfg(test)]
 mod leaser_tests;
 
-#[cfg(test)]
 mod lease_tests;
 
-#[cfg(test)]
 mod oracle_tests;
 
-#[cfg(test)]
 mod timealarms_tests;
+
+mod rust_runtime_tests;
