fd7f8024f74b13edcd06bd2e391367b73474c450
Gancho Manev <g.manev@nolus.io>
2023-03-03 09:28:47 +0200

    feat: switch to use the real USDC not Dai

--- packages/currency/src/lpn.rs
+++ packages/currency/src/lpn.rs
@@ -12,15 +12,10 @@ define_symbol! {
             dex: "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858",
         },
         alt: {
-            /// full ibc route: transfer/channel-0/transfer/channel-312/uausdc
-            // bank: "ibc/3DF30ED1342A40EA6E4941CBBE1FD4C7591F8DBA662D8162560E6A1429B018D4",
-            /// full ibc route: transfer/channel-0/transfer/channel-261/udws
-            bank: "ibc/9B44527BABE15E11BDB470A4B16E32A6F79D1DDE383B0687C2320B2C8614C309",
-            /// full ibc route: transfer/channel-312/uausdc
-            // dex: "ibc/75C8E3091D507A5A111C652F9C76C2E53059E24759A98B523723E02FA33EEF51",
-            /// full ibc route: transfer/channel-261/udws
-            /// borrowed from osmosis.pool_id = 672 as a currency with enough liquidity
-            dex: "ibc/902BDADA0D46931BF5DEBE0648CC1FE137AA4B7346475DD0490D503C937A12BD",
+            /// full ibc route: transfer/channel-0/transfer/channel-1946/uausdc
+            bank: "ibc/B37EE251476F383954F68EE99C2BFE1E78D45EE4AA792952A8C3545E3543A89F",
+            /// full ibc route: transfer/channel-1946/uausdc
+            dex: "ibc/FF3065989E34457F342D4EFB8692406D49D4E2B5C70F725F127862E22CE6BDCD",
         },
     }
 }
