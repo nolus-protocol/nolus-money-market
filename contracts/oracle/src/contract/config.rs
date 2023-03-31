nolus-protocol/nolus-money-market

commit 48916114916597efdf4cc2278040ad5d4a7d9651

fix: Removed storing of instantiating address as owner

contracts/oracle/src/contract/mod.rs
@@ -61,8 +61,6 @@ impl<'a> AnyVisitor for InstantiateWithCurrency<'a> {
     where
         C: Currency,
     {
-        SingleUserAccess::new_contract_owner(self.owner).store(self.deps.storage)?;
-
         self.msg.config.store(self.deps.storage)?;
 
         SupportedPairs::<C>::new(self.msg.swap_tree.into_tree())?

contracts/lpp/src/contract/mod.rs
@@ -42,7 +42,6 @@ impl<'a> InstantiateWithLpn<'a> {
     {
         versioning::initialize(self.deps.storage, version!(CONTRACT_STORAGE_VERSION))?;
 
-        SingleUserAccess::new_contract_owner(self.info.sender).store(self.deps.storage)?;
         SingleUserAccess::new(
             crate::access_control::LEASE_CODE_ADMIN_KEY,
             self.msg.lease_code_admin.clone(),

contracts/dispatcher/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "rewards_dispatcher"
-version = "0.2.0"
+version = "0.2.1"
 authors = ["The Dev Nolus Team <dev@nolus.io>"]
 edition.workspace = true
 license.workspace = true

contracts/dispatcher/src/contract.rs
@@ -41,7 +41,6 @@ pub fn instantiate(
     platform::contract::validate_addr(&deps.querier, &msg.timealarms)?;
     platform::contract::validate_addr(&deps.querier, &msg.treasury)?;
 
-    SingleUserAccess::new_contract_owner(info.sender).store(deps.storage)?;
     SingleUserAccess::new(
         crate::access_control::TIMEALARMS_NAMESPACE,
         msg.timealarms.clone(),

contracts/lpp/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "lpp"
-version = "0.3.0"
+version = "0.3.1"
 authors = ["The Dev Nolus Team <dev@nolus.io>"]
 edition.workspace = true
 license.workspace = true

contracts/oracle/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "oracle"
-version = "0.4.1"
+version = "0.4.2"
 authors = ["The Dev Nolus Team <dev@nolus.io>"]
 edition.workspace = true
 license.workspace = true

contracts/profit/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "profit"
-version = "0.2.0"
+version = "0.2.1"
 authors = ["The Dev Nolus Team <dev@nolus.io>"]
 edition.workspace = true
 license.workspace = true

contracts/profit/src/contract.rs
@@ -32,7 +32,6 @@ pub fn instantiate(
 
     versioning::initialize(deps.storage, version!(CONTRACT_STORAGE_VERSION))?;
 
-    SingleUserAccess::new_contract_owner(info.sender).store(deps.storage)?;
     SingleUserAccess::new(
         crate::access_control::TIMEALARMS_NAMESPACE,
         msg.timealarms.clone(),

commit 669078b3a7964ce028e4ed318703b5d6b26c7de8

fix: Fixed regression introduced in 48916114916597efdf4cc2278040ad5d4a7d9651

contracts/oracle/src/contract/config.rs
@@ -1,14 +1,12 @@
-use access_control::SingleUserAccess;
 use marketprice::config::Config as PriceConfig;
 use sdk::{cosmwasm_ext::Response, cosmwasm_std::Storage};
 
 use crate::{msg::ConfigResponse, state::config::Config, ContractError};
 
-pub fn query_config(storage: &dyn Storage) -> Result<ConfigResponse, ContractError> {
-    let owner = SingleUserAccess::load_contract_owner(storage)?.into();
+pub(super) fn query_config(storage: &dyn Storage) -> Result<ConfigResponse, ContractError> {
     let config = Config::load(storage)?;
 
-    Ok(ConfigResponse { owner, config })
+    Ok(ConfigResponse { config })
 }
 
 pub(super) fn try_configure(
@@ -50,7 +48,7 @@ mod tests {
             Percent::from_percent(50),
             swap_tree!((1, Cro::TICKER)),
         );
-        let (mut deps, info) = setup_test(msg);
+        let (mut deps, _info) = setup_test(msg);
 
         let msg = SudoMsg::UpdateConfig(PriceConfig::new(
             Percent::from_percent(44),
@@ -77,7 +75,6 @@ mod tests {
         assert_eq!(
             value,
             ConfigResponse {
-                owner: info.sender,
                 config: Config {
                     base_asset: Usdc::TICKER.into(),
                     price_config: PriceConfig::new(

contracts/oracle/src/contract/mod.rs
@@ -171,13 +171,12 @@ mod tests {
             Percent::from_percent(50),
             swap_tree!((1, Osmo::TICKER)),
         );
-        let (deps, info) = setup_test(msg);
+        let (deps, _info) = setup_test(msg);
 
         let res = query(deps.as_ref(), mock_env(), QueryMsg::Config {}).unwrap();
         let value: ConfigResponse = from_binary(&res).unwrap();
         assert_eq!(
             ConfigResponse {
-                owner: info.sender,
                 config: Config {
                     base_asset: Usdc::TICKER.into(),
                     price_config: PriceConfig::new(

contracts/oracle/src/contract/query.rs
@@ -4,8 +4,8 @@ use currency::lpn::Lpns;
 use finance::currency::{visit_any_on_ticker, AnyVisitor, AnyVisitorResult, Currency};
 use sdk::cosmwasm_std::{to_binary, Binary, Deps, Env};
 
-use super::oracle::Oracle;
 use crate::{
+    contract::oracle::Oracle,
     msg::{PricesResponse, QueryMsg, SwapTreeResponse},
     state::{config::Config, supported_pairs::SupportedPairs},
     ContractError,

contracts/oracle/src/msg.rs
@@ -99,7 +99,6 @@ pub type SupportedCurrencyPairsResponse = Vec<SwapLeg>;
 #[derive(Serialize, Deserialize, PartialEq, Eq, JsonSchema)]
 #[cfg_attr(any(test, feature = "testing"), derive(Debug, Clone))]
 pub struct ConfigResponse {
-    pub owner: Addr,
     pub config: Config,
 }
 