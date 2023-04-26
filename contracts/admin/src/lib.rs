5f6f78709f1bf544f4ccac27613b84cacd6b4938
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-04-26 12:50:49 +0300

    revert: Partially reverted fb9ac735950a7c010936895220da1d002d64809d

--- contracts/admin/src/lib.rs
+++ contracts/admin/src/lib.rs
@@ -41,8 +41,7 @@ pub fn instantiate(
 
 #[cfg_attr(feature = "contract-with-bindings", entry_point)]
 pub fn migrate(deps: DepsMut<'_>, _env: Env, _msg: MigrateMsg) -> ContractResult<CwResponse> {
-    versioning::update_software(deps.storage, version!(CONTRACT_STORAGE_VERSION))
-        .map_err(Into::into)
+    versioning::update_software::<ContractError>(deps.storage, version!(CONTRACT_STORAGE_VERSION))
         .and_then(|label| response::response(&label))
 }
 
