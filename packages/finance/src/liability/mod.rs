a6ff46a6b04efd0d0f55abf578aa33ceb0e19a16
Katerina Maneva <k.g.maneva@gmail.com>
2023-05-12 11:42:39 +0300

    Modified tests

--- packages/finance/src/liability/mod.rs
+++ packages/finance/src/liability/mod.rs
@@ -323,24 +323,28 @@ mod test {
 
     #[test]
     fn init_borrow() {
-        test_init_borrow_amount(1000, 10, 111, None);
+        test_init_borrow_amount(1000, 50, 1000, None);
         test_init_borrow_amount(1, 10, 0, None);
         test_init_borrow_amount(1000, 99, 990 * 100, None);
         test_init_borrow_amount(10, 65, 18, None);
-        test_init_borrow_amount(1, 65, 1, None);
-        test_init_borrow_amount(2, 65, 3, None);
+        test_init_borrow_amount(100, 60, 150, None);
+        test_init_borrow_amount(250, 59, 359, None);
+        test_init_borrow_amount(70, 5, 3, None);
+        test_init_borrow_amount(90, 25, 30, None);
     }
 
     #[test]
     fn init_borrow_max_ltd() {
         test_init_borrow_amount(50000, 60, 25000, Some(Percent::from_percent(50)));
-        test_init_borrow_amount(1000, 10, 50, Some(Percent::from_percent(5)));
+        test_init_borrow_amount(1000, 10, 100, Some(Percent::from_percent(10)));
         test_init_borrow_amount(1, 10, 0, Some(Percent::from_percent(5)));
-        test_init_borrow_amount(1000, 99, 800, Some(Percent::from_percent(80)));
-        test_init_borrow_amount(10, 65, 1, Some(Percent::from_percent(10)));
+        test_init_borrow_amount(1000, 60, 1500, Some(Percent::from_percent(190)));
+        test_init_borrow_amount(4000, 55, 4800, Some(Percent::from_percent(120)));
+        test_init_borrow_amount(200, 49, 192, Some(Percent::from_percent(100)));
         test_init_borrow_amount(1, 65, 0, Some(Percent::from_percent(65)));
-        test_init_borrow_amount(2, 65, 1, Some(Percent::from_percent(65)));
-        test_init_borrow_amount(2, 65, 1, Some(Percent::from_permille(999)));
+        test_init_borrow_amount(2000, 60, 3000, Some(Percent::from_percent(250)));
+        test_init_borrow_amount(300000, 65, 450000, Some(Percent::from_percent(150)));
+        test_init_borrow_amount(50, 45, 40, Some(Percent::from_permille(999)));
 
         test_init_borrow_amount(1000, 65, 0, Some(Percent::ZERO));
     }
@@ -412,8 +416,6 @@ mod test {
     }
 
     fn test_init_borrow_amount(d: u128, p: u16, exp: u128, max_p: Option<Percent>) {
-        use crate::fraction::Fraction as _;
-
         type Currency = Usdc;
 
         let downpayment = Coin::<Currency>::new(d);
@@ -429,11 +431,6 @@ mod test {
         }
         .init_borrow_amount(downpayment, max_p);
 
-        let default_ltd: Percent =
-            Percent::from_ratio(percent.units(), (Percent::HUNDRED - percent).units());
-        let selected_p = max_p.map_or(default_ltd, |max_p| max_p.min(percent));
-
         assert_eq!(calculated, Coin::<Currency>::new(exp));
-        assert_eq!(selected_p.of(downpayment), calculated);
     }
 }
