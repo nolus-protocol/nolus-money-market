e08ef288c09aa9e924185dd2ecd2b8f6c3a10fe4
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-06-05 11:52:14 +0300

    refactor(currency): Refactored macro to allow arbitrary network count

--- packages/currency/build.rs
+++ packages/currency/build.rs
@@ -1,5 +1,3 @@
 fn main() {
-    if option_env!("ALT_NET_SYMBOLS").map_or(false, |value| ["1", "y", "Y"].contains(&value)) {
-        println!("cargo:rustc-cfg=feature=\"alt_net_symbols\"");
-    }
+    println!("cargo:rustc-cfg=net_name=\"{}\"", env!("NET_NAME"));
 }
