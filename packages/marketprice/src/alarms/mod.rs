565b53123f50afe528ab0f033712abcaa7efef02
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-06-27 13:28:04 +0300

    refactor: Avoid repetition

--- packages/marketprice/src/alarms/mod.rs
+++ packages/marketprice/src/alarms/mod.rs
@@ -214,11 +214,8 @@ where
     pub fn remove_all(&mut self, subscriber: Addr) -> Result<(), AlarmError> {
         self.alarms_below
             .remove(self.storage.deref_mut(), subscriber.clone())
-            .and_then(|()| {
-                self.alarms_above_or_equal
-                    .remove(self.storage.deref_mut(), subscriber)
-            })
             .map_err(Into::into)
+            .and_then(|()| self.remove_above_or_equal(subscriber))
     }
 
     pub fn out_for_delivery(&mut self, subscriber: Addr) -> Result<(), AlarmError> {
