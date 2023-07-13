3afd89917aa114f25edb43a2057a4414343d3ad9
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-07-13 11:01:49 +0300

    refactor(tests/lease): Separate `close` functions

--- tests/src/lease_tests.rs
+++ tests/src/lease_tests.rs
@@ -363,16 +363,59 @@ mod close_mod {
         contract_addr: Addr,
         expected_funds: &[CwCoin],
     ) -> AppResponse {
-        send_close_and_transfer(test_case, &contract_addr, expected_funds);
+        let response: ResponseWithInterChainMsgs<'_, ()> = send_close(test_case, contract_addr.clone());
 
+        expect_remote_ibc_transfer(response);
+
+        do_remote_ibc_transfer(test_case, contract_addr, expected_funds)
+    }
+
+    fn send_close<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, TimeAlarms>(
+        test_case: &mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, TimeAlarms>,
+        contract_addr: Addr,
+    ) -> ResponseWithInterChainMsgs<'_, ()> {
+        test_case
+            .app
+            .execute(
+                Addr::unchecked(USER),
+                contract_addr,
+                &ExecuteMsg::Close {},
+                &[],
+            )
+            .unwrap()
+            .ignore_response()
+    }
+
+    fn expect_remote_ibc_transfer(
+        mut response: ResponseWithInterChainMsgs<'_, ()>,
+    ) {
+        response.expect_submit_tx(TestCase::LEASER_CONNECTION_ID, "0", 1);
+
+        () = response.unwrap_response()
+    }
+
+    fn do_remote_ibc_transfer<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, TimeAlarms>(
+        test_case: &mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, TimeAlarms>,
+        contract_addr: Addr,
+        funds: &[CwCoin],
+    ) -> AppResponse {
         assert_eq!(
             test_case
                 .app
                 .query()
                 .query_all_balances(contract_addr.clone())
                 .unwrap(),
-            expected_funds
+            &[] as &[CwCoin]
         );
+
+        test_case
+            .app
+            .send_tokens(
+                Addr::unchecked("ica0"),
+                contract_addr.clone(),
+                funds,
+            )
+            .unwrap();
 
         assert_eq!(
             test_case.app.query().query_all_balances("ica0").unwrap(),
@@ -385,36 +428,6 @@ mod close_mod {
             .sudo(contract_addr, &construct_response(Binary::default()))
             .unwrap()
             .unwrap_response()
-    }
-
-    fn send_close_and_transfer<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, TimeAlarms>(
-        test_case: &mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, TimeAlarms>,
-        contract_addr: &Addr,
-        expected_funds: &[CwCoin],
-    ) {
-        let mut response: ResponseWithInterChainMsgs<'_, ()> = test_case
-            .app
-            .execute(
-                Addr::unchecked(USER),
-                contract_addr.clone(),
-                &ExecuteMsg::Close {},
-                &[],
-            )
-            .unwrap()
-            .ignore_response();
-
-        response.expect_submit_tx(TestCase::LEASER_CONNECTION_ID, "0", 1);
-
-        () = response.unwrap_response();
-
-        test_case
-            .app
-            .send_tokens(
-                Addr::unchecked("ica0"),
-                contract_addr.clone(),
-                expected_funds,
-            )
-            .unwrap();
     }
 }
 
