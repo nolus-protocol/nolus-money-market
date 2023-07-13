a74b1ccad0dda82c95c6b3cc3d7c3a5015cd6ae8
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-07-13 11:01:40 +0300

    refactor(tests/lease): Separate `repay` functions

--- tests/src/lease_tests.rs
+++ tests/src/lease_tests.rs
@@ -213,8 +213,6 @@ fn construct_response(data: Binary) -> NeutronSudoMsg {
 }
 
 mod repay_mod {
-    use sdk::neutron_sdk::sudo::msg::SudoMsg;
-
     use super::*;
 
     pub(crate) fn repay<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
@@ -224,58 +222,34 @@ mod repay_mod {
     ) -> AppResponse {
         let cw_payment: CwCoin = cwcoin(payment);
 
-        send_payment_and_transfer(test_case, &contract_addr, cw_payment.clone());
+        let response: ResponseWithInterChainMsgs<'_, ()> =
+            send_payment_and_transfer(test_case, contract_addr.clone(), cw_payment.clone());
 
-        confirm_transfer_and_do_swap(test_case, &contract_addr, cw_payment, payment);
+        expect_swap(response);
 
-        confirm_swap_and_send_funds(test_case, &contract_addr, payment);
+        let swap_out_lpn: LpnCoin = price::total(payment, price_lpn_of());
 
-        confirm_transfer(test_case, contract_addr)
+        let response: ResponseWithInterChainMsgs<'_, ()> = do_swap(
+            test_case,
+            contract_addr.clone(),
+            &cw_payment,
+            swap_out_lpn,
+        );
+
+        expect_remote_ibc_transfer(response);
+
+        do_remote_ibc_transfer(
+            test_case,
+            contract_addr.clone(),
+            &cwcoin(swap_out_lpn),
+        )
     }
 
-    fn confirm_swap_and_send_funds<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
-        test_case: &mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, Addr>,
-        contract_addr: &Addr,
-        payment: Coin<Atom>,
-    ) {
-        let mut response: ResponseWithInterChainMsgs<'_, ()> = test_case
-            .app
-            .sudo(contract_addr.clone(), &construct_swap_response(payment))
-            .unwrap()
-            .ignore_response();
-
-        response.expect_submit_tx(TestCase::LEASER_CONNECTION_ID, "0", 1);
-
-        () = response.unwrap_response();
-
-        test_case
-            .app
-            .send_tokens(
-                Addr::unchecked("ica0"),
-                contract_addr.clone(),
-                &[cwcoin(price::total(payment, price_lpn_of()))],
-            )
-            .unwrap();
-    }
-
-    fn construct_swap_response(payment: PaymentCoin) -> SudoMsg {
-        construct_response(Binary(platform::trx::encode_msg_responses(
-            [platform::trx::encode_msg_response(
-                MsgSwapExactAmountInResponse {
-                    token_out_amount: Amount::from(price::total(payment, price_lpn_of()))
-                        .to_string(),
-                },
-                MsgSwapExactAmountIn::TYPE_URL,
-            )]
-            .into_iter(),
-        )))
-    }
-
-    fn send_payment_and_transfer<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
-        test_case: &mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, Addr>,
-        contract_addr: &Addr,
+    fn send_payment_and_transfer<'r, Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
+        test_case: &'r mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, Addr>,
+        contract_addr: Addr,
         cw_payment: CwCoin,
-    ) {
+    ) -> ResponseWithInterChainMsgs<'r, ()> {
         let mut response: ResponseWithInterChainMsgs<'_, ()> = test_case
             .app
             .execute(
@@ -304,46 +278,75 @@ mod repay_mod {
                 &[cw_payment],
             )
             .unwrap();
+
+        test_case
+            .app
+            .sudo(contract_addr, &construct_response(Binary::default()))
+            .unwrap()
+            .ignore_response()
     }
 
-    fn confirm_transfer_and_do_swap<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
-        test_case: &mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, Addr>,
-        contract_addr: &Addr,
-        cw_payment: CwCoin,
-        payment: PaymentCoin,
-    ) {
-        let mut response: ResponseWithInterChainMsgs<'_, ()> = test_case
-            .app
-            .sudo(
-                contract_addr.clone(),
-                &construct_response(Binary::default()),
-            )
-            .unwrap()
-            .ignore_response();
-
+    fn expect_swap(mut response: ResponseWithInterChainMsgs<'_, ()>) {
         response.expect_submit_tx(TestCase::LEASER_CONNECTION_ID, "0", 1);
 
-        () = response.unwrap_response();
+        response.unwrap_response()
+    }
 
+    fn do_swap<'r, Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
+        test_case: &'r mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, Addr>,
+        contract_addr: Addr,
+        cw_payment: &CwCoin,
+        swap_out_lpn: LpnCoin,
+    ) -> ResponseWithInterChainMsgs<'r, ()> {
         test_case
             .app
             .send_tokens(
                 Addr::unchecked("ica0"),
                 Addr::unchecked(ADMIN),
-                &[cw_payment],
+                std::slice::from_ref(cw_payment),
             )
             .unwrap();
 
-        test_case.send_funds_from_admin(
-            Addr::unchecked("ica0"),
-            &[cwcoin(price::total(payment, price_lpn_of()))],
-        );
+        test_case.send_funds_from_admin(Addr::unchecked("ica0"), &[cwcoin(swap_out_lpn)]);
+
+        test_case
+            .app
+            .sudo(
+                contract_addr,
+                &construct_response(Binary(platform::trx::encode_msg_responses(
+                    [platform::trx::encode_msg_response(
+                        MsgSwapExactAmountInResponse {
+                            token_out_amount: Amount::from(swap_out_lpn).to_string(),
+                        },
+                        MsgSwapExactAmountIn::TYPE_URL,
+                    )]
+                    .into_iter(),
+                ))),
+            )
+            .unwrap()
+            .ignore_response()
     }
 
-    fn confirm_transfer<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
+    fn expect_remote_ibc_transfer(mut response: ResponseWithInterChainMsgs<'_, ()>) {
+        response.expect_submit_tx(TestCase::LEASER_CONNECTION_ID, "0", 1);
+
+        response.unwrap_response()
+    }
+
+    fn do_remote_ibc_transfer<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle>(
         test_case: &mut TestCase<Dispatcher, Treasury, Profit, Leaser, Lpp, Oracle, Addr>,
         contract_addr: Addr,
+        cw_swap_out_lpn: &CwCoin,
     ) -> AppResponse {
+        test_case
+            .app
+            .send_tokens(
+                Addr::unchecked("ica0"),
+                contract_addr.clone(),
+                std::slice::from_ref(cw_swap_out_lpn),
+            )
+            .unwrap();
+
         test_case
             .app
             .sudo(contract_addr, &construct_response(Binary::default()))
