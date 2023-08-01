dd4108aff2a6b7f2aa7993797e7dfaa81f25b65b
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2023-08-01 15:10:01 +0300

    fix: Add types annotations in `oracle` macro

--- contracts/oracle/src/macros.rs
+++ contracts/oracle/src/macros.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! swap_tree {
     ($(($id: literal, $currency: expr $(,)?)),+ $(,)?) => {
-        serde_json_wasm::from_str(&tree::tree_json! {
+        serde_json_wasm::from_str::<::tree::HumanReadableTree<::swap::SwapTarget>>(&::tree::tree_json! {
             value: format!("[0, {:?}]", Usdc::TICKER),
             children: [
                 $({ value: format!("[{}, {:?}]", $id, $currency) }),+
