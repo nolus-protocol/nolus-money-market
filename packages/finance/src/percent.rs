15ec94773fc6db499a733adbafb89e7112ceee98
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2023-08-17 16:10:58 +0300

    feat: Added `BoundPercent` type

--- packages/finance/src/percent.rs
+++ packages/finance/src/percent.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 use sdk::{
     cosmwasm_std::{OverflowError, OverflowOperation},
@@ -166,6 +167,53 @@ impl<'a> Sub<&'a Percent> for Percent {
         self - *rhs
     }
 }
+
+impl<const UPPER_BOUND: Units> From<BoundPercent<UPPER_BOUND>> for Percent {
+    fn from(value: BoundPercent<UPPER_BOUND>) -> Self {
+        value.percent()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default, Serialize, Deserialize)]
+#[repr(transparent)]
+#[serde(into = "Percent", try_from = "Percent")]
+pub struct BoundPercent<const UPPER_BOUND: Units>(Percent);
+
+impl<const UPPER_BOUND: Units> BoundPercent<UPPER_BOUND> {
+    pub const fn try_from_percent(percent: Percent) -> Result<Self, UpperBoundCrossed> {
+        if percent.units() <= UPPER_BOUND {
+            Ok(Self(percent))
+        } else {
+            Err(UpperBoundCrossed {
+                bound: UPPER_BOUND,
+                value: percent.units(),
+            })
+        }
+    }
+
+    pub const fn percent(&self) -> Percent {
+        self.0
+    }
+}
+
+impl<const UPPER_BOUND: Units> TryFrom<Percent> for BoundPercent<UPPER_BOUND> {
+    type Error = UpperBoundCrossed;
+
+    fn try_from(value: Percent) -> Result<Self, Self::Error> {
+        Self::try_from_percent(value)
+    }
+}
+
+#[derive(Debug, Error)]
+#[error(
+    "[Finance] [Percent] Upper bound has been crossed! Upper bound is: {bound}, but got: {value}!"
+)]
+pub struct UpperBoundCrossed {
+    bound: Units,
+    value: Units,
+}
+
+pub type BoundToHundredPercent = BoundPercent<{ Percent::HUNDRED.units() }>;
 
 #[cfg(test)]
 pub(super) mod test {
