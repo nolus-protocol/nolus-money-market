49bc8b7621403b616dfeb86149c09659eac585d4
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2023-09-01 11:16:27 +0300

    refactor: Replace use of `gamm` module with `poolmanager`

--- packages/swap/src/trx.rs
+++ packages/swap/src/trx.rs
@@ -1,6 +1,5 @@
-use osmosis_std::types::osmosis::{
-    gamm::v1beta1::{MsgSwapExactAmountIn, MsgSwapExactAmountInResponse},
-    poolmanager::v1beta1::SwapAmountInRoute,
+use osmosis_std::types::osmosis::poolmanager::v1beta1::{
+    MsgSwapExactAmountIn, MsgSwapExactAmountInResponse, SwapAmountInRoute,
 };
 
 use currency::{self, Group, Symbol};
