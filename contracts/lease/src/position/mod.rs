362fb8a6a32117dd69a50cdc2ffbcc3758562d81
Katerina Maneva <k.g.maneva@gmail.com>
2023-10-02 19:10:08 +0300

    chore(position): Improve tests for check_liability

--- contracts/lease/src/position/mod.rs
+++ contracts/lease/src/position/mod.rs
@@ -226,6 +226,7 @@ mod test_check {
     use currency::{lpn::Usdc, test::Dai};
     use finance::{
         coin::Coin,
+        coin::Coin,
         duration::Duration,
         liability::{Liability, Zone},
         percent::Percent,
