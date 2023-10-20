49f097259c2063f154adfc438d085b590b32a9dd
Gancho Manev <g.manev@nolus.io>
2023-10-20 08:35:46 +0300

    build: set rerun if env changes

--- platform/packages/currency/build.rs
+++ platform/packages/currency/build.rs
@@ -1,15 +1,21 @@
 use std::env::{self, VarError};
 
+const NET_ENV_KEY: &str = "NET";
+const NET_CFG_KEY: &str = "net";
+const DEX_ENV_KEY: &str = "DEX";
+const DEX_CFG_KEY: &str = "dex";
+
 const TEST_NET: &str = "dev";
 const TEST_DEX: &str = "osmosis";
 
 fn main() {
+    rerun_on_env_change();
     if option_env("CARGO_FEATURE_TESTING").is_some() {
         enable_net_to_dex(TEST_NET, TEST_DEX)
     } else if option_env("CARGO_FEATURE_IMPL").is_some() {
         enable_net_to_dex(
-            &option_env("NET").expect("NET=[dex|test|main] should be set"),
-            &option_env("DEX").expect("DEX=osmosis should be set"),
+            &option_env(NET_ENV_KEY).expect("NET=[dev|test|main] should be set"),
+            &option_env(DEX_ENV_KEY).expect("DEX=osmosis should be set"),
         )
     }
 }
@@ -23,6 +29,19 @@ fn option_env(key: &str) -> Option<String> {
 }
 
 fn enable_net_to_dex(net: &str, dex: &str) {
-    println!("cargo:rustc-cfg=net=\"{}\"", net);
-    println!("cargo:rustc-cfg=dex=\"{}\"", dex);
+    enable_cfg(NET_CFG_KEY, net);
+    enable_cfg(DEX_CFG_KEY, dex);
+}
+
+fn enable_cfg(key: &str, cfg: &str) {
+    println!("cargo:rustc-cfg={}=\"{}\"", key, cfg);
+}
+
+fn rerun_on_env_change() {
+    rerun_if_env_changes(NET_ENV_KEY);
+    rerun_if_env_changes(DEX_ENV_KEY);
+}
+
+fn rerun_if_env_changes(env_key: &str) {
+    println!("cargo:rerun-if-env-changed={}", env_key);
 }
