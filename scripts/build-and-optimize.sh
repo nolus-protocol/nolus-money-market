2bba396d93ffb5bbfdadddb80607c2a952bc68d8
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2023-10-24 23:07:52 +0300

    ci: Refactored `build-and-optimize.sh` script

--- scripts/build-and-optimize.sh
+++ scripts/build-and-optimize.sh
@@ -10,25 +10,38 @@ for contract in "/code/contracts/"*
 do
     cd "${contract}"
 
+    contract_pkgid="$(cargo pkgid)"
+    contract_pkgid="${contract_pkgid##*/}"
+    contract_pkgid_filter_at="${contract_pkgid%%@*}"
+    if test "${contract_pkgid}" = "${contract_pkgid_filter_at}"
+    then
+        contract_pkgid="${contract_pkgid%%#*}"
+    else
+        contract_pkgid="${contract_pkgid_filter_at##*#}"
+    fi
+    unset "contract_pkgid_filter_at"
+
     cargo build --release --lib --locked --target wasm32-unknown-unknown --target-dir "/target/"
+
+    if ! test $? -eq 0
+    then
+        echo "[ERROR] Cargo exitted with non-zero status code while being ran against \"${contract_pkgid}\"!"
+
+        exit 1
+    fi
+
+    wasm-opt -Os --signext-lowering -o "/artifacts/${contract_pkgid}.wasm" "/target/wasm32-unknown-unknown/release/${contract_pkgid}.wasm"
+
+    if ! test $? -eq 0
+    then
+        echo "[ERROR] \"wasm-opt\" exitted with non-zero status code while being ran against \"${contract_pkgid}\"!"
+
+        exit 1
+    fi
 done
 
-if test $? -eq 0
-then
-    rm -rf "/artifacts/"*
+echo "\nChecksums:"
 
-    for wasm in "/target/wasm32-unknown-unknown/release/"*".wasm"
-    do
-        wasm_name=$(basename "${wasm}" ".wasm")
-
-        echo "[INFO] Processing \"${wasm_name}\" through \"wasm-opt\"..."
-
-        wasm-opt -Os --signext-lowering -o "/artifacts/${wasm_name}.wasm" "${wasm}"
-    done
-
-    sha256sum -- "/artifacts/"*".wasm" | tee "/artifacts/checksums.txt"
-else
-    echo "[ERROR] Cargo exitted with non-zero status code!"
-fi
+sha256sum -- "/artifacts/"*".wasm" | tee "/artifacts/checksums.txt"
 
 rm -rf "/target/"
