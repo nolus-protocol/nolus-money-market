340587c3b4d731493302a7547aace65cf757d32c
Gancho Manev <g.manev@nolus.io>
2023-11-14 17:03:28 +0200

    fmt

--- protocol/contracts/lease/src/api/open/mod.rs
+++ protocol/contracts/lease/src/api/open/mod.rs
@@ -143,7 +143,12 @@ pub struct PositionSpecDTO {
 
 #[cfg(feature = "skel")]
 impl PositionSpecDTO {
-    #[cfg(any(test, feature = "astroport", feature = "osmosis", feature = "migration"))]
+    #[cfg(any(
+        test,
+        feature = "astroport",
+        feature = "osmosis",
+        feature = "migration"
+    ))]
     pub(crate) fn new_internal(
         liability: Liability,
         min_asset: LpnCoin,
