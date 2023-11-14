bc8b46e77e16c72507100f9c247a1194c25c6e41
Gancho Manev <g.manev@nolus.io>
2023-11-14 15:28:39 +0200

    fix(tests): test messages in Astroport feature

--- protocol/packages/swap/src/trx/test.rs
+++ protocol/packages/swap/src/trx/test.rs
@@ -1,8 +1,14 @@
-#[cfg(dex = "osmosis")]
+#[cfg(feature = "astroport")]
+mod msg {
+
+    pub type RequestMsg = crate::trx::astroport::RequestMsg;
+
+    pub type ResponseMsg = crate::trx::astroport::ResponseMsg;
+}
+#[cfg(all(not(feature = "astroport"), feature = "osmosis"))]
 mod msg {
 
     pub type RequestMsg = crate::trx::osmosis::RequestMsg;
 
     pub type ResponseMsg = crate::trx::osmosis::ResponseMsg;
-}
-pub use msg::*;
+}pub use msg::*;
