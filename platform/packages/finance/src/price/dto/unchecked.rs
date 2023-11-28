092e5786f0b66e6218d9bf2fff74e011914a2572
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2023-11-28 16:45:06 +0200

    feat: Added check for group

--- platform/packages/finance/src/price/dto/unchecked.rs
+++ platform/packages/finance/src/price/dto/unchecked.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-use currency::Group;
+use currency::{visit_any_on_tickers, AnyVisitorPair, AnyVisitorPairResult, Group};
 
 use crate::{coin::CoinDTO, error::Error};
 
@@ -30,11 +30,31 @@ where
     type Error = Error;
 
     fn try_from(dto: PriceDTO<G, QuoteG>) -> Result<Self, Self::Error> {
+        visit_any_on_tickers::<G, QuoteG, _>(
+            dto.amount.ticker(),
+            dto.amount_quote.ticker(),
+            VisitorImpl,
+        )?;
+
         let res = Self {
             amount: dto.amount,
             amount_quote: dto.amount_quote,
         };
+
         res.invariant_held()?;
+
         Ok(res)
+    }
+}
+
+struct VisitorImpl;
+
+impl AnyVisitorPair for VisitorImpl {
+    type Output = ();
+
+    type Error = Error;
+
+    fn on<C1, C2>(self) -> AnyVisitorPairResult<Self> {
+        Ok(())
     }
 }
