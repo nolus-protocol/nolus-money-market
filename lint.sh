8a6b44cac33d78570da5fb7d20dccf0b771e3453
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2023-11-28 10:49:42 +0200

    ci: Updated CI actions to reflect changes & optimized

--- lint.sh
+++ lint.sh
@@ -2,6 +2,6 @@
 
 # -D warnings is set so we allow 'deprecated' lints to tolerate them
 # remove '-A deprecated' to find out if we use any
-cargo clippy --workspace --verbose --all-targets -- -D warnings -D future-incompatible \
+cargo clippy --workspace --verbose --all-targets --features "$1" -- -D warnings -D future-incompatible \
   -D nonstandard-style -D rust-2018-compatibility -D rust-2018-idioms \
   -D rust-2021-compatibility -D unused -D clippy::all
