0e2079d8211cb80636c2111168caa272c2ad74bf
Kiril Mihaylov <80464733+kirilmihaylov@users.noreply.github.com>
2024-01-04 10:43:49 +0200

    fix: Converted docstrings into regular comments

--- protocol/packages/currencies/src/lpn/astroport.rs
+++ protocol/packages/currencies/src/lpn/astroport.rs
@@ -1,27 +1,26 @@
-use sdk::schemars;
-
 use currency::{AnyVisitor, Matcher, MaybeAnyVisitResult, SymbolSlice};
+use sdk::schemars;
 
 use crate::{define_currency, define_symbol};
 
 define_symbol! {
     USDC_AXELAR {
         ["net_dev"]: {
-            /// full ibc route: transfer/channel-116/transfer/channel-8/uausdc
+            // full ibc route: transfer/channel-116/transfer/channel-8/uausdc
             bank: "ibc/B3F73CBDD3C286B8EA46FA9100A114B91731F0F4A23660FBAA47DCB7AAA968AB",
-            /// full ibc route: transfer/channel-8/uausdc
+            // full ibc route: transfer/channel-8/uausdc
             dex: "ibc/F91EA2C0A23697A1048E08C2F787E3A58AC6F706A1CD2257A504925158CFC0F3",
         },
         ["net_test"]: {
-            /// full ibc route: transfer/channel-1990/transfer/channel-8/uausdc
+            // full ibc route: transfer/channel-1990/transfer/channel-8/uausdc
             bank: "ibc/88E889952D6F30CEFCE1B1EE4089DA54939DE44B0A7F11558C230209AF228937",
-            /// full ibc route: transfer/channel-8/uausdc
+            // full ibc route: transfer/channel-8/uausdc
             dex: "ibc/F91EA2C0A23697A1048E08C2F787E3A58AC6F706A1CD2257A504925158CFC0F3",
         },
         ["net_main"]: {
-            /// full ibc route: transfer/channel-3839/transfer/channel-2/uusdc
+            // full ibc route: transfer/channel-3839/transfer/channel-2/uusdc
             bank: "ibc/076CF690A9912E0B7A2CCA75B719D68AF7C20E4B0B6460569B333DDEB19BBBA1",
-            /// full ibc route: transfer/channel-2/uusdc
+            // full ibc route: transfer/channel-2/uusdc
             dex: "ibc/F082B65C88E4B6D5EF1DB243CDA1D331D002759E938A0F5CD3FFDC5D53B3E349",
         },
     }
--- protocol/packages/currencies/src/native/osmosis.rs
+++ protocol/packages/currencies/src/native/osmosis.rs
@@ -6,17 +6,17 @@ define_symbol! {
     NLS {
         ["net_dev"]: {
             bank: "unls",
-            /// full ibc route: transfer/channel-109/unls
+            // full ibc route: transfer/channel-109/unls
             dex: "ibc/5E7589614F0B4B80D91923D15D8EB0972AAA6226F7566921F1D6A07EA0DB0D2C"
         },
         ["net_test"]: {
             bank: "unls",
-            /// full ibc route: transfer/channel-4508/unls
+            // full ibc route: transfer/channel-4508/unls
             dex: "ibc/1588A50E9EF2B6E45B443B8AF5AD7891996D7104566908982603D73D1956FE51"
         },
         ["net_main"]: {
             bank: "unls",
-            /// full ibc route: transfer/channel-783/unls
+            // full ibc route: transfer/channel-783/unls
             dex: "ibc/D9AFCECDD361D38302AA66EB3BAC23B95234832C51D12489DC451FA2B7C72782"
         },
     }
