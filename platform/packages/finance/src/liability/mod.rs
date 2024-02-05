691d48f9d7b2a6d3be727b13ce68b08aae8fbee1
Gancho Manev <g.manev@nolus.io>
2024-02-05 17:15:36 +0200

    fix(ci): formatting and feature dependencies

--- platform/packages/finance/src/liability/mod.rs
+++ platform/packages/finance/src/liability/mod.rs
@@ -22,7 +22,11 @@ mod zone;
 
 #[derive(Serialize, Deserialize, Copy, Clone, Eq, PartialEq, JsonSchema)]
 #[cfg_attr(any(test, feature = "testing"), derive(Debug))]
-#[serde(deny_unknown_fields, rename_all = "snake_case", try_from = "unchecked::Liability")]
+#[serde(
+    deny_unknown_fields,
+    rename_all = "snake_case",
+    try_from = "unchecked::Liability"
+)]
 pub struct Liability {
     /// The initial percentage of the amount due versus the locked collateral
     /// initial > 0
