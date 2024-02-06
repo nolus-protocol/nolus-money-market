a59cebcdd90492a04b691add3fdfc5759d03a0b1
Gancho Manev <g.manev@nolus.io>
2024-02-06 11:17:55 +0200

    fix(loan): get away from using serde(untagged)

--- protocol/contracts/lease/src/loan/unchecked.rs
+++ protocol/contracts/lease/src/loan/unchecked.rs
@@ -13,54 +13,54 @@ use crate::api::open::InterestPaymentSpec;
 use super::LoanDTO as LastVersionLoanDTO;
 
 /// Migrates v0.4.2 LoanDTO instance to the next v0.5.0 format
+/// TODO clean-up the v0.4.2 support once all leases have gone through this migration
 #[derive(Deserialize)]
-#[serde(untagged)] // TODO clean-up the v0.4.2 support once all leases have gone through this migration
-pub(super) enum LoanDTO {
-    V0_5_0 {
-        // this is the first variant since the untagged enum representation tries to deserialize to variants as per their order in the definition
-        lpp: LppRef,
-        profit: ProfitRef,
-        due_period: Duration,
-        margin_interest: Percent,
-        margin_paid_by: Timestamp, // only this one should vary!
-    },
-    V0_4_2 {
-        lpp: LppRef,
-        interest_payment_spec: InterestPaymentSpec,
-        current_period: InterestPeriod<Units, Percent>,
-        profit: ProfitRef,
-    },
+pub(super) struct LoanDTO {
+    lpp: LppRef,
+    profit: ProfitRef,
+    // v0.5.0 fields follow
+    #[serde(default)]
+    due_period: Duration,
+    #[serde(default)]
+    margin_interest: Percent,
+    #[serde(default)]
+    margin_paid_by: Timestamp,
+    // v0.4.2 fields follow
+    interest_payment_spec: Option<InterestPaymentSpec>,
+    current_period: Option<InterestPeriod<Units, Percent>>,
 }
 
 impl From<LoanDTO> for LastVersionLoanDTO {
     fn from(value: LoanDTO) -> Self {
-        match value {
-            LoanDTO::V0_4_2 {
-                lpp,
-                interest_payment_spec,
-                current_period,
-                profit,
-            } => LastVersionLoanDTO {
-                lpp,
-                profit,
-                due_period: interest_payment_spec.due_period(),
+        if value.interest_payment_spec.is_some() {
+            assert!(value.current_period.is_some());
+            assert_eq!(value.margin_paid_by, Timestamp::default());
+
+            // v0.4.2 detected
+            let current_period = value
+                .current_period
+                .expect("Current period to be present in v0.4.2 data");
+            Self {
+                lpp: value.lpp,
+                profit: value.profit,
+                due_period: value
+                    .interest_payment_spec
+                    .expect("Interest payment spec to be present in v0.4.2 data")
+                    .due_period(),
                 margin_interest: current_period.interest_rate(),
                 margin_paid_by: current_period.start(),
-            },
+            }
+        } else {
+            assert_ne!(value.margin_paid_by, Timestamp::default());
 
-            LoanDTO::V0_5_0 {
-                lpp,
-                profit,
-                due_period,
-                margin_interest,
-                margin_paid_by,
-            } => LastVersionLoanDTO {
-                lpp,
-                profit,
-                due_period,
-                margin_interest,
-                margin_paid_by,
-            },
+            // v0.5.0 detected
+            LastVersionLoanDTO {
+                lpp: value.lpp,
+                profit: value.profit,
+                due_period: value.due_period,
+                margin_interest: value.margin_interest,
+                margin_paid_by: value.margin_paid_by,
+            }
         }
     }
 }
@@ -69,6 +69,7 @@ impl From<LoanDTO> for LastVersionLoanDTO {
 mod test_two_versions {
 
     use cosmwasm_std::Timestamp;
+    use currency::Currency;
     use finance::{duration::Duration, percent::Percent};
     use lpp::stub::LppRef;
     use profit::stub::ProfitRef;
@@ -76,14 +77,8 @@ mod test_two_versions {
 
     use crate::loan::{tests::Lpn, LoanDTO};
 
-    #[test]
-    fn read_4_2_into_5_0() {
-        const RAW_4_2: &str = r#"{"lpp":{"addr":"nolus1qg5ega6dykkxc307y25pecuufrjkxkaggkkxh7nad0vhyhtuhw3sqaa3c5","currency":"USDC"},
-                                "profit":{"addr":"nolus1mf6ptkssddfmxvhdx0ech0k03ktp6kf9yk59renau2gvht3nq2gqkxgywu"},
-                                "interest_payment_spec":{"due_period":604800000000000,"grace_period":172800000000000},
-                                "current_period":{"period":{"start":"1706820166180052443","length":0},"interest":40}}"#;
-
-        let loan_v5_0 = LoanDTO {
+    fn loan_v5_0() -> LoanDTO {
+        LoanDTO {
             lpp: LppRef::unchecked::<_, Lpn>(
                 "nolus1qg5ega6dykkxc307y25pecuufrjkxkaggkkxh7nad0vhyhtuhw3sqaa3c5",
             ),
@@ -93,15 +88,33 @@ mod test_two_versions {
             due_period: Duration::from_nanos(604800000000000),
             margin_interest: Percent::from_permille(40),
             margin_paid_by: Timestamp::from_nanos(1706820166180052443),
-        };
-
-        assert_eq!(loan_v5_0, from_json(RAW_4_2).unwrap());
-        {
-            assert_eq!(
-                loan_v5_0,
-                from_json(to_json_vec(&loan_v5_0).expect("serialization passed"))
-                    .expect("deserialization passed")
-            );
         }
+    }
+
+    #[test]
+    fn read_4_2_into_5_0() {
+        let raw_4_2 = format!(
+            r#"{{"lpp":{{"addr":"nolus1qg5ega6dykkxc307y25pecuufrjkxkaggkkxh7nad0vhyhtuhw3sqaa3c5","currency":{:?}}},
+        "profit":{{"addr":"nolus1mf6ptkssddfmxvhdx0ech0k03ktp6kf9yk59renau2gvht3nq2gqkxgywu"}},
+        "interest_payment_spec":{{"due_period":604800000000000,"grace_period":172800000000000}},
+        "current_period":{{"period":{{"start":"1706820166180052443","length":0}},"interest":40}}}}"#,
+            Lpn::TICKER
+        );
+
+        assert_eq!(loan_v5_0(), from_json(raw_4_2.clone()).unwrap());
+        assert_eq!(
+            to_json_vec(&loan_v5_0()).expect("serialization passed"),
+            to_json_vec(&from_json::<LoanDTO>(raw_4_2).expect("deserialization passed"))
+                .expect("serialization passed")
+        );
+    }
+
+    #[test]
+    fn read_5_0() {
+        assert_eq!(
+            loan_v5_0(),
+            from_json(to_json_vec(&loan_v5_0()).expect("serialization passed"))
+                .expect("deserialization passed")
+        );
     }
 }
