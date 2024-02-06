1aec046adbeb73c6f9ec20f1432a5d776f2f3adc
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-02-06 17:18:38 +0200

    test: Added response integration test

---
+++ protocol/packages/swap/tests/request.rs
@@ -0,0 +1,60 @@
+use currency::{
+    test::{SubGroup, SubGroupTestC1, SuperGroup, SuperGroupTestC2, SuperGroupTestC3},
+    Currency as _,
+};
+use dex::swap::{ExactAmountIn, SwapRequest};
+use oracle::api::swap::SwapTarget;
+use platform::trx::Transaction;
+use sdk::{
+    cosmos_sdk_proto::Any as CosmosAny, cosmwasm_std::Binary,
+    neutron_sdk::bindings::types::ProtobufAny as NeutronAny,
+};
+use swap::Impl;
+
+#[test]
+fn build_and_parse() {
+    let expected_token_in =
+        finance::coin::from_amount_ticker::<SubGroup>(20, SubGroupTestC1::TICKER.into()).unwrap();
+
+    let expected_swap_path = vec![
+        SwapTarget {
+            pool_id: 0,
+            target: SuperGroupTestC2::TICKER.into(),
+        },
+        SwapTarget {
+            pool_id: 0,
+            target: SuperGroupTestC3::TICKER.into(),
+        },
+    ];
+
+    let request: CosmosAny = {
+        let mut tx = Transaction::default();
+
+        <Impl as ExactAmountIn>::build_request::<SubGroup, SuperGroup>(
+            &mut tx,
+            String::from("host_account").try_into().unwrap(),
+            &expected_token_in,
+            &expected_swap_path,
+        )
+        .unwrap();
+
+        let mut msgs = tx.into_iter();
+
+        let NeutronAny {
+            type_url,
+            value: Binary(value),
+        } = msgs.next().unwrap();
+
+        assert!(msgs.next().is_none());
+
+        CosmosAny { type_url, value }
+    };
+
+    let SwapRequest {
+        token_in,
+        swap_path,
+    } = <Impl as ExactAmountIn>::parse_request::<SubGroup, SuperGroup>(request);
+
+    assert_eq!(token_in, expected_token_in);
+    assert_eq!(swap_path, expected_swap_path);
+}
