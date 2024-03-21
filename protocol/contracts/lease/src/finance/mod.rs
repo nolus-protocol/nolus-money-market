937d55e3853bc979f14e0558d0824318074e92fe
Gancho Manev <g.manev@nolus.io>
2024-03-21 08:58:26 +0200

    chore(lease): remove unused code

--- protocol/contracts/lease/src/finance/mod.rs
+++ protocol/contracts/lease/src/finance/mod.rs
@@ -5,8 +5,6 @@ use finance::coin::CoinDTO;
 
 pub type LpnCoinDTO = CoinDTO<LpnCurrencies>;
 
-pub type LppRef = lpp::stub::LppRef<LpnCurrency, LpnCurrencies>;
-
 pub type ReserveRef = reserve::stub::Ref<LpnCurrency>;
 
 #[cfg(feature = "contract")]
