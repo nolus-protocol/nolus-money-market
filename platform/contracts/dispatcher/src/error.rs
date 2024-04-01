c3a7461935a873662906de8bb11fb98c9354eabe
Gancho Manev <g.manev@nolus.io>
2024-04-01 18:01:32 +0300

    chore(dispatcher): cleanup dependencies and errors

--- platform/contracts/dispatcher/src/error.rs
+++ platform/contracts/dispatcher/src/error.rs
@@ -1,8 +1,6 @@
-use std::convert::Infallible;
-
 use thiserror::Error;
 
-use sdk::cosmwasm_std::{Addr, StdError};
+use sdk::cosmwasm_std::StdError;
 
 #[derive(Error, PartialEq, Debug)]
 pub enum ContractError {
@@ -10,41 +8,5 @@ pub enum ContractError {
     Std(#[from] StdError),
 
     #[error("[Dispatcher] {0}")]
-    LppPlatformError(#[from] lpp_platform::error::Error),
-
-    #[error("[Dispatcher] {0}")]
     Platform(#[from] platform::error::Error),
-
-    #[error("[Dispatcher] {0}")]
-    Oracle(#[from] oracle_platform::error::Error),
-
-    #[error("[Dispatcher] {0}")]
-    Finance(#[from] finance::error::Error),
-
-    #[error("[Dispatcher] {0}")]
-    TimeAlarm(#[from] timealarms::error::ContractError),
-
-    #[error("[Dispatcher] [Infallible] {0}")]
-    FromInfallible(#[from] Infallible),
-
-    #[error("[Dispatcher] {0}")]
-    Unauthorized(#[from] access_control::error::Error),
-
-    #[error("[Dispatcher] Unknown currency symbol: {symbol:?}")]
-    UnknownCurrency { symbol: String },
-
-    #[error("[Dispatcher] Invalid contract address {0}")]
-    InvalidContractAddress(Addr),
-
-    #[error("[Dispatcher] Invalid alarm notification address: {0:?}")]
-    InvalidAlarmAddress(Addr),
-
-    #[error("[Dispatcher] Alarm comming from unknown address: {0:?}")]
-    UnrecognisedAlarm(Addr),
-
-    #[error("[Dispatcher] Invalid time configuration. Current reward distribution time is before the last distribution time")]
-    InvalidTimeConfiguration {},
-
-    #[error("[Dispatcher] Zero Reward")]
-    ZeroReward {},
 }
