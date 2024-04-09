9673cd8f707053d7d491a29c29374176af542a90
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-04-09 16:42:08 +0300

    refactor(admin): Make schema reflect changes

--- platform/contracts/admin/examples/admin_schema.rs
+++ platform/contracts/admin/examples/admin_schema.rs
@@ -1,7 +1,5 @@
-#[cfg(feature = "migrate")]
-use admin_contract::msg::MigrateMsg;
 use admin_contract::msg::{
-    ExecuteMsg, InstantiateMsg, PlatformQueryResponse, ProtocolQueryResponse,
+    ExecuteMsg, InstantiateMsg, MigrateMsg, PlatformQueryResponse, ProtocolQueryResponse,
     ProtocolsQueryResponse, QueryMsg, SudoMsg,
 };
 use sdk::cosmwasm_schema::{export_schema, schema_for};
@@ -10,7 +8,6 @@ fn main() {
     let out_dir = schema::prep_out_dir().expect("The output directory should be valid");
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    #[cfg(feature = "migrate")]
     export_schema(&schema_for!(MigrateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(SudoMsg), &out_dir);
