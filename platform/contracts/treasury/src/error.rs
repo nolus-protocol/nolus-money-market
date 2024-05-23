7cf6e96fe468ea69041284da64f4b5dc0c85cac4
Gancho Manev <g.manev@nolus.io>
2024-05-23 13:11:39 +0300

    refactor(treasury): error messages now refer to the right contract

--- platform/contracts/treasury/src/error.rs
+++ platform/contracts/treasury/src/error.rs
@@ -4,63 +4,63 @@ use sdk::cosmwasm_std::StdError;
 
 #[derive(Error, PartialEq, Debug)]
 pub enum ContractError {
-    #[error("[Dispatcher] Failed to serialize! Cause: {0}")]
+    #[error("[Treasury] Failed to serialize! Cause: {0}")]
     Serialize(StdError),
 
-    #[error("[Dispatcher] Failed to init the contract version! Cause: {0}")]
+    #[error("[Treasury] Failed to init the contract version! Cause: {0}")]
     InitVersion(StdError),
 
-    #[error("[Dispatcher] Failed to validate the Registry address! Cause: {0}")]
+    #[error("[Treasury] Failed to validate the Registry address! Cause: {0}")]
     ValidateRegistryAddr(StdError),
 
-    #[error("[Dispatcher] Failed to validate the Timealarms address! Cause: {0}")]
+    #[error("[Treasury] Failed to validate the Timealarms address! Cause: {0}")]
     ValidateTimeAlarmsAddr(platform::error::Error),
 
-    #[error("[Dispatcher] Failed to update the storage! Cause: {0}")]
+    #[error("[Treasury] Failed to update the storage! Cause: {0}")]
     UpdateStorage(StdError),
 
-    #[error("[Dispatcher] Failed to update the software! Cause: {0}")]
+    #[error("[Treasury] Failed to update the software! Cause: {0}")]
     UpdateSoftware(StdError),
 
-    #[error("[Dispatcher] Failed to retrieve all protocols! Cause: {0}")]
+    #[error("[Treasury] Failed to retrieve all protocols! Cause: {0}")]
     QueryProtocols(StdError),
 
-    #[error("[Dispatcher] Failed to protocol contracts! Cause: {0}")]
+    #[error("[Treasury] Failed to protocol contracts! Cause: {0}")]
     QueryProtocol(StdError),
 
-    #[error("[Dispatcher] {0}")]
+    #[error("[Treasury] {0}")]
     SerializeResponse(#[from] platform::error::Error),
 
-    #[error("[Dispatcher] Failed to load the configuration! Cause: {0}")]
+    #[error("[Treasury] Failed to load the configuration! Cause: {0}")]
     LoadConfig(StdError),
 
-    #[error("[Dispatcher] Failed to save the configuration! Cause: {0}")]
+    #[error("[Treasury] Failed to save the configuration! Cause: {0}")]
     SaveConfig(StdError),
 
-    #[error("[Dispatcher] Failed to load the dispatch log! Cause: {0}")]
+    #[error("[Treasury] Failed to load the dispatch log! Cause: {0}")]
     LoadDispatchLog(StdError),
 
-    #[error("[Dispatcher] Failed to save the dispatch log! Cause: {0}")]
+    #[error("[Treasury] Failed to save the dispatch log! Cause: {0}")]
     SaveDispatchLog(StdError),
 
-    #[error("[Dispatcher] Failed to obtain Lpp balance! Cause: {0}")]
+    #[error("[Treasury] Failed to obtain Lpp balance! Cause: {0}")]
     ReadLppBalance(lpp_platform::error::Error),
 
-    #[error("[Dispatcher] Failed to distribute rewards to an Lpp! Cause: {0}")]
+    #[error("[Treasury] Failed to distribute rewards to an Lpp! Cause: {0}")]
     DistributeLppReward(lpp_platform::error::Error),
 
-    #[error("[Dispatcher] Failed to convert rewards to NLS! Cause: {0}")]
+    #[error("[Treasury] Failed to convert rewards to NLS! Cause: {0}")]
     ConvertRewardsToNLS(oracle_platform::error::Error),
 
-    #[error("[Dispatcher] Failed to setup a time alarms stub! Cause: {0}")]
+    #[error("[Treasury] Failed to setup a time alarms stub! Cause: {0}")]
     SetupTimeAlarmStub(timealarms::error::ContractError),
 
-    #[error("[Dispatcher] Failed to setup a time alarm! Cause: {0}")]
+    #[error("[Treasury] Failed to setup a time alarm! Cause: {0}")]
     SetupTimeAlarm(timealarms::error::ContractError),
 
-    #[error("[Dispatcher] {0}")]
+    #[error("[Treasury] {0}")]
     Unauthorized(#[from] access_control::error::Error),
 
-    #[error("[Dispatcher] Invalid time configuration. Current reward distribution time is before the last distribution time")]
+    #[error("[Treasury] Invalid time configuration. Current reward distribution time is before the last distribution time")]
     InvalidTimeConfiguration {},
 }
