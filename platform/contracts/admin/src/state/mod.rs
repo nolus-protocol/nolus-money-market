d2f129fd52ea53a1ccb4f4523ca3cf03d7798a31
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-06-17 13:58:14 +0300

    feat(admin): Replace full migration with software one

--- platform/contracts/admin/src/state/mod.rs
+++ platform/contracts/admin/src/state/mod.rs
@@ -1,10 +1,2 @@
-use sdk::cosmwasm_std::Storage;
-
-use crate::result::Result;
-
 pub(crate) mod contract;
 pub(crate) mod contracts;
-
-pub(crate) fn migrate(storage: &mut dyn Storage) -> Result<()> {
-    contracts::migrate_platform(storage)
-}
