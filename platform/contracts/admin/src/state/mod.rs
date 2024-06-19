cd237a9636b56bcdeeb31cee37a92388d91014e0
Gancho Manev <g.manev@nolus.io>
2024-06-19 12:53:23 +0300

    Revert "Merge pull request #357 from nolus-protocol/feat(admin)-replace-full-migration"
    This reverts commit b7ccd875775a0c8de0b55bf44e4bd34bb20086f5, reversing
    changes made to 30e64a6c3fa0a1ed459ca14cabcb4f3e1bc59481.

--- platform/contracts/admin/src/state/mod.rs
+++ platform/contracts/admin/src/state/mod.rs
@@ -1,2 +1,10 @@
+use sdk::cosmwasm_std::Storage;
+
+use crate::result::Result;
+
 pub(crate) mod contract;
 pub(crate) mod contracts;
+
+pub(crate) fn migrate(storage: &mut dyn Storage) -> Result<()> {
+    contracts::migrate_platform(storage)
+}
