5f7b44ed50fbb52964307e0e904c99e2622ed7f4
Katerina Maneva <k.g.maneva@gmail.com>
2024-06-21 10:34:51 +0300

    chore: Formatting

--- platform/packages/finance/src/price/base/mod.rs
+++ platform/packages/finance/src/price/base/mod.rs
@@ -14,8 +14,8 @@ use crate::{
 use super::{dto::PriceDTO, Price};
 
 mod unchecked;
-pub mod with_quote;
 pub mod with_price;
+pub mod with_quote;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Eq, JsonSchema)]
 #[serde(
