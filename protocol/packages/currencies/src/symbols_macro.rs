106cd039661c02724caf07558115fa9dd5e69d0e
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-06-28 11:17:37 +0300

    fix(currencies): Fix up `define_symbol` macro

--- protocol/packages/currencies/src/symbols_macro.rs
+++ protocol/packages/currencies/src/symbols_macro.rs
@@ -6,14 +6,10 @@ pub struct CurrencySymbols {
 #[macro_export]
 macro_rules! define_symbol {
     (
-        $currency: ident {
-            $({ $body:tt }* ),+ $(,)?
-        } $(,)?
+        $currency: ident { $($body:tt)* } $(,)?
     ) => {
         pub const $currency: $crate::symbols_macro::CurrencySymbols = {
-            $(
-                { $crate::symbols_macro::CurrencySymbols { $($body)* } }
-            )+
+            $crate::symbols_macro::CurrencySymbols { $($body)* }
         };
     };
 }
