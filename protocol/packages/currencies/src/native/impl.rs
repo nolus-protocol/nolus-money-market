583f98fc1ad7b05744b3b4cee5bdbbfdbc62fa5c
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-06-28 11:26:44 +0300

    style: Manual formatting on macro

--- protocol/packages/currencies/src/native/impl.rs
+++ protocol/packages/currencies/src/native/impl.rs
@@ -2,9 +2,9 @@ use crate::{define_currency, define_symbol, Native};
 
 define_symbol! {
     NLS {
-            bank: "unls",
-            // full ibc route: transfer/channel-783/unls
-            dex: "ibc/D9AFCECDD361D38302AA66EB3BAC23B95234832C51D12489DC451FA2B7C72782"
+        bank: "unls",
+        // full ibc route: transfer/channel-783/unls
+        dex: "ibc/D9AFCECDD361D38302AA66EB3BAC23B95234832C51D12489DC451FA2B7C72782"
     }
 }
 define_currency!(Nls, NLS, Native, 6);
--- protocol/packages/currencies/src/payment/only/impl.rs
+++ protocol/packages/currencies/src/payment/only/impl.rs
@@ -4,10 +4,10 @@ use crate::{define_currency, define_symbol, PaymentOnlyGroup};
 
 define_symbol! {
     USDC_AXELAR {
-            // full ibc route: transfer/channel-0/transfer/channel-208/uusdc
-            bank: "ibc/7FBDBEEEBA9C50C4BCDF7BF438EAB99E64360833D240B32655C96E319559E911",
-            // full ibc route: transfer/channel-208/uusdc
-            dex: "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858",
+        // full ibc route: transfer/channel-0/transfer/channel-208/uusdc
+        bank: "ibc/7FBDBEEEBA9C50C4BCDF7BF438EAB99E64360833D240B32655C96E319559E911",
+        // full ibc route: transfer/channel-208/uusdc
+        dex: "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858",
     }
 }
 define_currency!(UsdcAxelar, USDC_AXELAR, PaymentOnlyGroup, 6);
