c9cdfb389fa89dfaa13f83465105b613d2b1db3d
Gancho Manev <g.manev@nolus.io>
2024-07-08 15:21:08 +0300

    fix(currencies): missed USDC_NOBLE and build rependencies

--- protocol/packages/currencies/src_by_protocol/neutron-astroport-usdc_axelar/net_main/payment/only/impl.rs
+++ protocol/packages/currencies/src_by_protocol/neutron-astroport-usdc_axelar/net_main/payment/only/impl.rs
@@ -3,6 +3,16 @@ use sdk::schemars;
 
 use crate::{define_currency, define_symbol};
 
+define_symbol! {
+    USDC_NOBLE {
+        // full ibc route: transfer/channel-3839/transfer/channel-30/uusdc
+        bank: "ibc/18161D8EFBD00FF5B7683EF8E923B8913453567FBE3FB6672D75712B0DEB6682",
+        // full ibc route: transfer/channel-30/uusdc
+        dex: "ibc/B559A80D62249C8AA07A380E2A2BEA6E5CA9A6F079C912C3A9E9B494105E4F81",
+    }
+}
+define_currency!(UsdcNoble, USDC_NOBLE, 6);
+
 pub(super) fn maybe_visit<M, V>(
     matcher: &M,
     symbol: &SymbolSlice,
@@ -26,30 +36,27 @@ mod test {
         test_impl::{
             maybe_visit_on_bank_symbol_err, maybe_visit_on_bank_symbol_impl,
             maybe_visit_on_ticker_err, maybe_visit_on_ticker_impl,
-        },
+        }, PaymentGroup,
     };
+
+    use super::UsdcNoble;
 
     #[test]
     fn maybe_visit_on_ticker() {
-        maybe_visit_on_ticker_err::<Nls, PaymentOnlyGroup>(Nls::TICKER);
-        maybe_visit_on_ticker_err::<Nls, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_ticker_err::<Lpn, PaymentOnlyGroup>(Lpn::TICKER);
-        maybe_visit_on_ticker_err::<Lpn, Lpns>(Nls::TICKER);
-
-        maybe_visit_on_ticker_err::<Nls, PaymentGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_ticker_err::<Lpn, PaymentGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_ticker_err::<Lpn, PaymentGroup>(Lpn::BANK_SYMBOL);
+        maybe_visit_on_ticker_impl::<UsdcNoble, PaymentOnlyGroup>();
+        
+        maybe_visit_on_ticker_err::<UsdcNoble, PaymentOnlyGroup>(UsdcNoble::BANK_SYMBOL);
+        maybe_visit_on_ticker_err::<UsdcNoble, PaymentOnlyGroup>(Lpn::TICKER);
+        maybe_visit_on_ticker_err::<Lpn, Lpns>(UsdcNoble::TICKER);
     }
 
     #[test]
     fn maybe_visit_on_bank_symbol() {
-        maybe_visit_on_bank_symbol_err::<Nls, PaymentOnlyGroup>(Nls::TICKER);
-        maybe_visit_on_bank_symbol_err::<Nls, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_bank_symbol_err::<Lpn, PaymentOnlyGroup>(Lpn::TICKER);
-        maybe_visit_on_bank_symbol_err::<Lpn, Lpns>(Nls::TICKER);
+        maybe_visit_on_bank_symbol_impl::<UsdcNoble, PaymentOnlyGroup>();
 
-        maybe_visit_on_bank_symbol_impl::<Nls, PaymentGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_bank_symbol_impl::<Lpn, PaymentGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_bank_symbol_impl::<Lpn, PaymentGroup>(Lpn::BANK_SYMBOL);
+        maybe_visit_on_bank_symbol_err::<UsdcNoble, PaymentOnlyGroup>(UsdcNoble::TICKER);
+        maybe_visit_on_bank_symbol_err::<UsdcNoble, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
+        maybe_visit_on_bank_symbol_err::<UsdcNoble, PaymentOnlyGroup>(Lpn::BANK_SYMBOL);
+        maybe_visit_on_bank_symbol_err::<Lpn, Lpns>(UsdcNoble::BANK_SYMBOL);
     }
 }
