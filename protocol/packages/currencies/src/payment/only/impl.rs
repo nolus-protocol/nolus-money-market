192e10397a0fb08ac058a9bdacf29d3e423819d4
Gancho Manev <g.manev@nolus.io>
2024-07-11 14:36:47 +0300

    chore(currency): remove mistakenly added implementation modules

--- protocol/packages/currencies/src/payment/only/impl.rs
+++
@@ -1,9 +0,0 @@
-use currency::{AnyVisitor, Matcher, MaybeAnyVisitResult};
-
-pub(super) fn maybe_visit<M, V>(_matcher: &M, visitor: V) -> MaybeAnyVisitResult<V>
-where
-    M: Matcher + ?Sized,
-    V: AnyVisitor,
-{
-    currency::visit_noone(visitor)
-}
