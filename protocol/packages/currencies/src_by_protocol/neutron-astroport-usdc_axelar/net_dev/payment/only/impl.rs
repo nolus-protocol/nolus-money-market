5260eafc435f1327e98eb58a43214038b4c087cf
Gancho Manev <g.manev@nolus.io>
2024-07-16 18:16:40 +0300

    tests(currencies): fix tests

--- protocol/packages/currencies/src_by_protocol/neutron-astroport-usdc_axelar/net_dev/payment/only/impl.rs
+++ protocol/packages/currencies/src_by_protocol/neutron-astroport-usdc_axelar/net_dev/payment/only/impl.rs
@@ -14,31 +14,25 @@ mod test {
 
     use crate::{
         lpn::{Lpn, Lpns},
-        native::Nls,
+        native::{Nls, Native},
         payment::only::PaymentOnlyGroup,
         test_impl::{maybe_visit_on_bank_symbol_err, maybe_visit_on_ticker_err},
     };
 
     #[test]
     fn maybe_visit_on_ticker() {
-        maybe_visit_on_ticker_err::<Nls, PaymentOnlyGroup>(Nls::TICKER);
-        maybe_visit_on_ticker_err::<Nls, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_ticker_err::<Lpn, PaymentOnlyGroup>(Lpn::TICKER);
+        maybe_visit_on_ticker_err::<Nls, Native>(Nls::TICKER);
+        maybe_visit_on_ticker_err::<Nls, Native>(Nls::BANK_SYMBOL);
         maybe_visit_on_ticker_err::<Lpn, Lpns>(Nls::TICKER);
 
-        maybe_visit_on_ticker_err::<Nls, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_ticker_err::<Lpn, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_ticker_err::<Lpn, PaymentOnlyGroup>(Lpn::BANK_SYMBOL);
+        maybe_visit_on_ticker_err::<Nls, Native>(Nls::BANK_SYMBOL);
+        maybe_visit_on_ticker_err::<Lpn, Lpns>(Lpn::BANK_SYMBOL);
     }
 
     #[test]
     fn maybe_visit_on_bank_symbol() {
-        maybe_visit_on_bank_symbol_err::<Nls, PaymentOnlyGroup>(Nls::TICKER);
-        maybe_visit_on_bank_symbol_err::<Nls, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_bank_symbol_err::<Lpn, PaymentOnlyGroup>(Lpn::TICKER);
+        maybe_visit_on_bank_symbol_err::<Nls, Native>(Nls::TICKER);
+        maybe_visit_on_bank_symbol_err::<Nls, Native>(Nls::BANK_SYMBOL);
         maybe_visit_on_bank_symbol_err::<Lpn, Lpns>(Nls::TICKER);
-
-        maybe_visit_on_bank_symbol_err::<Lpn, PaymentOnlyGroup>(Nls::BANK_SYMBOL);
-        maybe_visit_on_bank_symbol_err::<Lpn, PaymentOnlyGroup>(Lpn::BANK_SYMBOL);
     }
 }
