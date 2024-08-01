37d5dab613f0dd6339770ee3c55f7bcc4d54c3c8
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-08-01 16:15:41 +0300

    refactor(tools/topology): Refactor `symbol::Ibc` for easier testability.

--- tools/topology/src/symbol.rs
+++ tools/topology/src/symbol.rs
@@ -57,6 +57,15 @@ struct Ibc {
 }
 
 impl Ibc {
+    const SHA2_256_OUTPUT_SIZE: usize = 32;
+
+    const BYTE_HALVES_PER_CHAR: usize = 2;
+
+    const SYMBOL_PREFIX: &'static str = "ibc/";
+
+    const OUTPUT_SYMBOL_LENGTH: usize =
+        Self::SYMBOL_PREFIX.len() + (Self::SHA2_256_OUTPUT_SIZE * Self::BYTE_HALVES_PER_CHAR);
+
     fn new(outmost_channel: &str) -> Self {
         let mut instance = Self {
             path: String::new(),
@@ -78,29 +87,58 @@ impl Ibc {
     fn add_symbol(mut self, symbol: &str) -> Symbol {
         self.path.push_str(symbol);
 
-        // "ibc/" + 64 hexadecimal digits
+        let symbol = Self::digest_symbol(&self.path);
+
+        Symbol {
+            path: self.path.into_boxed_str(),
+            symbol,
+        }
+    }
+
+    fn digest_symbol(path: &str) -> Box<str> {
         let mut symbol = String::new();
 
-        symbol.reserve_exact(68);
+        symbol.reserve_exact(Self::OUTPUT_SYMBOL_LENGTH);
 
-        symbol.push_str("ibc/");
+        symbol.push_str(Self::SYMBOL_PREFIX);
 
-        Sha256::digest(&self.path)
+        let digest: [u8; Self::SHA2_256_OUTPUT_SIZE] = Sha256::digest(&path).into();
+
+        Self::map_into_hex_iter(digest).for_each(|ch| symbol.push(ch));
+
+        symbol.into()
+    }
+
+    fn map_into_hex_iter<const N: usize>(digest: [u8; N]) -> impl Iterator<Item = char> {
+        Self::map_into_byte_halves(digest)
             .into_iter()
-            .flat_map(|byte| [byte >> 4, byte & 0xF])
+            .flat_map(|ByteHalves { low, high }| [high, low])
             .map(|half_byte| {
                 if half_byte < 10 {
                     b'0' + half_byte
                 } else {
                     b'A' + (half_byte - 10)
                 }
+                .into()
             })
-            .map(char::from)
-            .for_each(|ch| symbol.push(ch));
+    }
 
-        Symbol {
-            path: self.path.into_boxed_str(),
-            symbol: symbol.into_boxed_str(),
-        }
+    fn map_into_byte_halves<const N: usize>(digest: [u8; N]) -> [ByteHalves; N] {
+        digest.map(|byte| ByteHalves {
+            high: byte >> 4,
+            low: byte & 0xF,
+        })
     }
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ByteHalves {
+    low: u8,
+    high: u8,
+}
+
+const _: () = {
+    let 68 = Ibc::OUTPUT_SYMBOL_LENGTH else {
+        panic!("Output symbol length didn't match well-known value!")
+    };
+};
