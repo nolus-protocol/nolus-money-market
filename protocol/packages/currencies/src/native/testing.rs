e89627209735c7594ce1139e3f1ece9273d6e65d
Gancho Manev <g.manev@nolus.io>
2024-08-16 09:21:14 +0300

    chore(currencies): cargo fmt

--- protocol/packages/currencies/src/native/testing.rs
+++ protocol/packages/currencies/src/native/testing.rs
@@ -2,11 +2,4 @@ use sdk::schemars;
 
 use crate::{define_currency, Native};
 
-define_currency!(
-    Nls,
-    "NLS",
-    "unls",
-    "ibc/test_DEX_NLS",
-    Native,
-    6,
-);
+define_currency!(Nls, "NLS", "unls", "ibc/test_DEX_NLS", Native, 6,);
