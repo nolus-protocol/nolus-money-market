3e397a89c300d72c5702334ed1ec0d968be0e628
desislavva <dessislv28th@gmail.com>
2024-09-25 15:04:43 +0300

    fix(currencies): osmosis-osmosis-osmo denoms

--- protocol/packages/currencies/src_by_protocol/osmosis-osmosis-osmo/net_test/lease/impl.rs
+++ protocol/packages/currencies/src_by_protocol/osmosis-osmosis-osmo/net_test/lease/impl.rs
@@ -14,8 +14,8 @@ use crate::{define_currency, LeaseGroup, Lpn, Nls, PaymentGroup};
 define_currency!(
     UsdcAxelar,
     "USDC_AXELAR",
-    "ibc/7FBDBEEEBA9C50C4BCDF7BF438EAB99E64360833D240B32655C96E319559E911", // transfer/channel-0/transfer/channel-208/uusdc
-    "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858", // transfer/channel-208/uusdc
+    "ibc/5DE4FCAF68AE40F81F738C857C0D95F7C1BC47B00FA1026E85C1DD92524D4A11", // transfer/channel-0/transfer/channel-3/uausdc
+    "ibc/6F34E1BD664C36CE49ACC28E60D62559A5F96C4F9A6CCE4FC5A67B2852E24CFE", // transfer/channel-3/uausdc
     LeaseGroup,
     6
 );
