a3e41549bd3a684422aa56a1b9c6377b8d0ac216
Gancho Manev <g.manev@nolus.io>
2024-09-27 13:31:36 +0300

    fix(leaser): lint

--- protocol/contracts/leaser/src/migrate.rs
+++ protocol/contracts/leaser/src/migrate.rs
@@ -39,7 +39,7 @@ where
 
     customers
         .find_map(|maybe_customer| match maybe_customer {
-            Ok(customer) => msgs.migrate_or_be_next(customer, &|| migrate_msg()),
+            Ok(customer) => msgs.migrate_or_be_next(customer, &migrate_msg),
             Err(err) => Some(Err(err)),
         })
         .transpose()
