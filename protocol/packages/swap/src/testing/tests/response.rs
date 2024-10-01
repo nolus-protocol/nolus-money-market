bcd40cbf496b7cdbbcc5217d6e2c90ebdebc6008
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-10-01 18:41:56 +0300

    refactor(protocol/swap): Move-out `validate_a_response`.

--- protocol/packages/swap/src/testing/tests/response.rs
+++ protocol/packages/swap/src/testing/tests/response.rs
@@ -1,6 +1,5 @@
-use dex::swap::{Error, ExactAmountIn};
+use dex::swap::ExactAmountIn;
 use finance::coin::Amount;
-use platform::trx;
 
 use crate::{testing::ExactAmountInSkel, Impl};
 
@@ -15,25 +14,4 @@ fn build_and_parse() {
     assert_eq!(parsed, expected_amount);
 
     assert_eq!(resp.next(), None);
-}
-
-pub(crate) fn validate(resp_base64: &str, exp_amount1: Amount, exp_amount2: Amount) {
-    use base64::{engine::general_purpose, Engine};
-
-    let resp = general_purpose::STANDARD.decode(resp_base64).unwrap();
-    let mut resp_messages = trx::decode_msg_responses(&resp).unwrap();
-
-    assert_eq!(
-        exp_amount1,
-        Impl::parse_response(&mut resp_messages).unwrap()
-    );
-    assert_eq!(
-        exp_amount2,
-        Impl::parse_response(&mut resp_messages).unwrap()
-    );
-    assert!(matches!(
-        Impl::parse_response(&mut resp_messages).unwrap_err(),
-        Error::MissingResponse(_)
-    ));
-    assert_eq!(None, resp_messages.next());
 }
