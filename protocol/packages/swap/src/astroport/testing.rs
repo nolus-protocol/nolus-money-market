bb89c24d54db305bb20df98b0469ef8db053bdc8
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-10-01 16:24:03 +0300

    refactor(protocol/swap): Revamp Astroport implementation to not utilize generic parameters.

--- protocol/packages/swap/src/astroport/testing.rs
+++ protocol/packages/swap/src/astroport/testing.rs
@@ -6,7 +6,6 @@ use astroport::{
 };
 
 use currency::{CurrencyDTO, Group, MemberOf};
-use dex::swap::ExactAmountIn;
 use finance::coin::{Amount, CoinDTO};
 use oracle::api::swap::{SwapPath, SwapTarget};
 use sdk::{
@@ -20,12 +19,11 @@ use sdk::{
 
 use crate::testing::{self, ExactAmountInSkel, SwapRequest};
 
-use super::{RequestMsg, ResponseMsg, Router, RouterImpl};
+use super::{Impl, RequestMsg, ResponseMsg, Router};
 
-impl<R> ExactAmountInSkel for RouterImpl<R>
+impl ExactAmountInSkel for Impl
 where
-    Self: ExactAmountIn,
-    R: Router,
+    Self: Router,
 {
     fn parse_request<GIn, GSwap>(request: CosmosAny) -> SwapRequest<GIn, GSwap>
     where
@@ -41,7 +39,7 @@ where
 
         assert_eq!(
             contract,
-            R::ROUTER_ADDR,
+            Self::ADDRESS,
             "Expected message to be addressed to currently selected router!"
         );
 
