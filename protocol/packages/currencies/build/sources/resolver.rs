db873b1073a0289780090d0dad448d4ff6e31911
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-11-07 14:54:36 +0200

    chore(protocol/currencies/build): Remove unused module.

--- protocol/packages/currencies/build/sources/resolver.rs
+++
@@ -1,7 +0,0 @@
-use anyhow::Result;
-
-use super::resolved_currency::ResolvedCurrency;
-
-pub(super) trait Resolver<'name, 'definition> {
-    fn resolve(&self, ticker: &str) -> Result<ResolvedCurrency<'name, 'definition>>;
-}
