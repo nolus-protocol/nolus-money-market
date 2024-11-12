891c7460a5ca868d607539b70c086b5a5749bd9d
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-11-12 17:01:27 +0200

    refactor(protocol/currencies): Reexport currencies in a separate `testing` module.

--- protocol/packages/currencies/src/lib.rs
+++ protocol/packages/currencies/src/lib.rs
@@ -1,5 +1,3 @@
-#[cfg(feature = "testing")]
-pub use self::payment::*;
 pub use self::{
     lease::Group as LeaseGroup,
     lpn::{Group as Lpns, Lpn},
@@ -13,6 +11,15 @@ mod lpn;
 mod native;
 mod payment;
 mod stable;
-
+#[cfg(feature = "testing")]
+pub mod testing {
+    pub use crate::{
+        lease::{LeaseC1, LeaseC2, LeaseC3, LeaseC4, LeaseC5, LeaseC6, LeaseC7},
+        payment::{
+            PaymentC1, PaymentC2, PaymentC3, PaymentC4, PaymentC5, PaymentC6, PaymentC7, PaymentC8,
+            PaymentC9,
+        },
+    };
+}
 #[cfg(test)]
 mod test_impl;
