45109f3b1eec81eb42fa7c79b65994284b4cab0a
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-11-13 11:43:01 +0200

    refactor(protocol/currencies): Revert to importing `schemers` instead.

--- protocol/packages/currencies/src/payment/mod.rs
+++ protocol/packages/currencies/src/payment/mod.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 use currency::{AnyVisitor, Matcher, MaybeAnyVisitResult, MemberOf};
-use sdk::schemars::JsonSchema;
+use sdk::schemars::{self, JsonSchema};
 
 use crate::{lease::Group as LeaseGroup, lpn::Group as LpnGroup, native::Group as NativeGroup};
 
@@ -20,7 +20,6 @@ mod testing;
     Clone, Copy, Debug, Ord, PartialEq, PartialOrd, Eq, Serialize, Deserialize, JsonSchema,
 )]
 #[serde(deny_unknown_fields, rename_all = "snake_case")]
-#[schemars(crate = "sdk::schemars")]
 pub enum Group {}
 
 impl currency::Group for Group {
