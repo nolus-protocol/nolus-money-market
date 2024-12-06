f817df684bf781cb1de66b41ff8ac36e833c13da
Gancho Manev <g.manev@nolus.io>
2024-12-06 09:36:44 +0200

    chore(test): patch a compilation issue

---
+++ tests/src/lease/close_policy.rs
@@ -0,0 +1,8 @@
+#[test]
+fn tp_zero() {
+    // let mut test_case = lease_mod::create_test_case::<PaymentCurrency>();
+    // let lease = lease_mod::open_lease(&mut test_case, DOWNPAYMENT, None);
+
+    // let response: AppResponse =
+    //     deliver_new_price(&mut test_case, lease, base, quote).unwrap_response();
+}
