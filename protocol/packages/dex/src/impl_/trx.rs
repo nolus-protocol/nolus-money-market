9fdd4f1d3a171c82e84a3aac26a28b41b9f1fed4
Gancho Manev <g.manev@nolus.io>
2024-12-06 18:05:43 +0200

    feat(dex): prepare the IBC timeout to accomodate the forthcoming IBC channels migration

--- protocol/packages/dex/src/impl_/trx.rs
+++ protocol/packages/dex/src/impl_/trx.rs
@@ -16,7 +16,7 @@ use sdk::cosmwasm_std::{Addr, QuerierWrapper, Timestamp};
 
 use crate::{error::Result, swap::ExactAmountIn};
 
-pub(super) const IBC_TIMEOUT: Duration = Duration::from_minutes(7); // less than the total amount of time 100 blocks take (a Hermes setting to look back for packets)
+pub(super) const IBC_TIMEOUT: Duration = Duration::from_days(100); //enough for the IBC channels migration to complete
 
 //TODO take them as input from the client
 const ICA_TRANSFER_ACK_TIP: Coin<Nls> = Coin::new(1);
