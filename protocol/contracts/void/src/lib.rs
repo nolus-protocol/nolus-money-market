74231402af58c9b15529788eee1a8e7b0260779d
Gancho Manev <g.manev@nolus.io>
2024-12-13 14:46:43 +0200

    refactor(versioning): rename ReleaseLabel to Release (cont.)

--- protocol/contracts/void/src/lib.rs
+++ protocol/contracts/void/src/lib.rs
@@ -8,7 +8,7 @@ use sdk::{
     cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo},
 };
 use timealarms::msg::ExecuteAlarmMsg;
-use versioning::{self, ReleaseLabel};
+use versioning::{self, Release};
 
 #[derive(Serialize, Deserialize)]
 pub struct EmptyMsg {}
@@ -29,7 +29,7 @@ pub fn migrate(
     _env: Env,
     EmptyMsg {}: EmptyMsg,
 ) -> Result<CwResponse, platform::error::Error> {
-    response::response(ReleaseLabel::void())
+    response::response(Release::void())
 }
 
 #[entry_point]
