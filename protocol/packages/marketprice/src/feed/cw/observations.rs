df0e7e8a837711efb542b58579bf6614899dc8f5
Gancho Manev <g.manev@nolus.io>
2024-12-19 14:30:21 +0200

    fix(marketprice): reorder the oldest observation

--- protocol/packages/marketprice/src/feed/cw/observations.rs
+++ protocol/packages/marketprice/src/feed/cw/observations.rs
@@ -82,7 +82,7 @@ where
                     if let Some(item) = may_item {
                         if item.valid_since(valid_since) {
                             self.storage
-                                .push_back(self.store.deref_mut(), &item)
+                                .push_front(self.store.deref_mut(), &item)
                                 .map_err(PriceFeedsError::FeedPush)
                                 .map(|()| false)
                         } else {
@@ -103,5 +103,97 @@ where
         self.storage
             .push_back(self.store.deref_mut(), &observation)
             .map_err(PriceFeedsError::FeedPush)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::ops::Deref;
+
+    use currency::test::{SuperGroupTestC4, SuperGroupTestC5};
+    use finance::{
+        coin::{Amount, Coin},
+        duration::Duration,
+        price::{self, Price},
+    };
+    use sdk::cosmwasm_std::{testing::MockStorage, Addr, Storage, Timestamp};
+
+    use crate::feed::observations::{Observations, ObservationsRead};
+
+    use super::{Deque, Observation};
+
+    const VALIDITY_PERIOD: Duration = Duration::from_secs(60);
+    const BLOCK_TIME: Timestamp = Timestamp::from_seconds(150);
+    const FEEDER: &str = "feeder1";
+
+    type C1 = SuperGroupTestC4;
+    type C2 = SuperGroupTestC5;
+
+    #[test]
+    fn retain() {
+        let mut store = MockStorage::new();
+        let mut deque = Deque::new(&mut store as &mut dyn Storage, "");
+
+        let feed1_time = BLOCK_TIME - VALIDITY_PERIOD;
+        let feed1_price = price(19, 5100);
+        let observation1 = Observation::new(Addr::unchecked(FEEDER), feed1_time, feed1_price);
+        deque.register(observation1.clone()).unwrap();
+        assert_eq!(1, deque.len());
+        assert_eq!(Some(Ok(observation1)), deque.as_iter().unwrap().next());
+
+        deque
+            .retain(&(feed1_time - Duration::from_nanos(1)))
+            .unwrap();
+        assert_eq!(1, deque.len());
+
+        deque.retain(&feed1_time).unwrap();
+        assert_eq!(0, deque.len());
+    }
+
+    #[test]
+    fn retain_order() {
+        let mut store = MockStorage::new();
+        let mut deque = Deque::new(&mut store as &mut dyn Storage, "");
+
+        let feed1_time = BLOCK_TIME - VALIDITY_PERIOD;
+        let feed1_price = price(19, 5100);
+        let observation1 = Observation::new(Addr::unchecked(FEEDER), feed1_time, feed1_price);
+        deque.register(observation1.clone()).unwrap();
+
+        let feed2_time = feed1_time + VALIDITY_PERIOD;
+        let feed2_price = price(20, 5100);
+        let observation2 = Observation::new(Addr::unchecked(FEEDER), feed2_time, feed2_price);
+        deque.register(observation2.clone()).unwrap();
+
+        assert_eq!(2, deque.len());
+        assert_elements(&deque, observation1.clone(), observation2.clone());
+
+        deque
+            .retain(&(feed1_time - Duration::from_nanos(1)))
+            .unwrap();
+        assert_eq!(2, deque.len());
+        assert_elements(&deque, observation1.clone(), observation2.clone());
+
+        deque.retain(&feed1_time).unwrap();
+        assert_eq!(1, deque.len());
+
+        deque.retain(&feed2_time).unwrap();
+        assert_eq!(0, deque.len());
+    }
+
+    fn assert_elements<'storage, S>(
+        deque: &Deque<'storage, C1, C2, S>,
+        observation1: Observation<C1, C2>,
+        observation2: Observation<C1, C2>,
+    ) where
+        S: Deref<Target = dyn Storage + 'storage>,
+    {
+        let mut it = deque.as_iter().unwrap();
+        assert_eq!(Some(Ok(observation1)), it.next());
+        assert_eq!(Some(Ok(observation2)), it.next());
+    }
+
+    fn price(c: Amount, q: Amount) -> Price<C1, C2> {
+        price::total_of(Coin::from(c)).is(Coin::from(q))
     }
 }
