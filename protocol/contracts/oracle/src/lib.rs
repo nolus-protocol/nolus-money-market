ebc663bfec740d2ba84592b7f2a453203ba78f86
Lockwarr <denislav66@gmail.com>
2025-01-29 15:01:58 +0200

    wip

--- protocol/contracts/oracle/src/lib.rs
+++ protocol/contracts/oracle/src/lib.rs
@@ -8,7 +8,7 @@ pub mod result;
 #[cfg(feature = "contract")]
 pub mod state;
 pub mod stub;
-#[cfg(feature = "stub_swap")]
+#[cfg(all(feature = "stub_swap", test))]
 pub mod test_tree;
 #[cfg(all(feature = "contract", test))]
 mod tests;
