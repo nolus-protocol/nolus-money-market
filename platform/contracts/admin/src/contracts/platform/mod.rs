86fee62a9199ed115cb72c58ff79541e832f26cd
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2025-02-07 12:15:26 +0200

    refactor(platform/admin): Add counterpart platform contracts template and split platform migration from execute.

--- platform/contracts/admin/src/contracts/platform/mod.rs
+++ platform/contracts/admin/src/contracts/platform/mod.rs
@@ -4,23 +4,44 @@ use sdk::schemars::{self, JsonSchema};
 
 use super::higher_order_type::FirstOrderType;
 
+pub(super) mod higher_order_type;
 #[cfg(feature = "contract")]
 mod impl_mod;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "snake_case", deny_unknown_fields)]
-pub struct Contracts<T> {
+pub struct ContractsWithoutAdmin<T> {
     pub timealarms: T,
     pub treasury: T,
 }
 
-impl<T> FirstOrderType<HigherOrderType> for Contracts<T> {
+impl<T> ContractsWithoutAdmin<T> {
+    pub fn with_admin(self, admin: T) -> Contracts<T> {
+        let Self {
+            timealarms,
+            treasury,
+        } = self;
+
+        Contracts {
+            admin,
+            timealarms,
+            treasury,
+        }
+    }
+}
+
+impl<T> FirstOrderType<higher_order_type::ContractsWithoutAdmin> for ContractsWithoutAdmin<T> {
     type Unit = T;
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, JsonSchema)]
-pub enum HigherOrderType {}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
+#[serde(rename_all = "snake_case", deny_unknown_fields)]
+pub struct Contracts<T> {
+    pub admin: T,
+    pub timealarms: T,
+    pub treasury: T,
+}
 
-impl super::HigherOrderType for HigherOrderType {
-    type Of<Unit> = Contracts<Unit>;
+impl<T> FirstOrderType<higher_order_type::Contracts> for Contracts<T> {
+    type Unit = T;
 }
