7f722d99ec6879fa4deedc7eff4af6c38f8f1b20
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2025-02-11 18:11:09 +0200

    refactor(platform/versioning): Remove unused public method.

--- platform/packages/versioning/src/lib.rs
+++ platform/packages/versioning/src/lib.rs
@@ -1,5 +1,3 @@
-use sdk::cosmwasm_std::Storage;
-
 #[cfg(feature = "schema")]
 pub use crate::software::SemVer;
 pub use crate::{
@@ -28,35 +26,4 @@ pub type ProtocolMigrationMessage<ContractMsg> =
 pub struct FullUpdateOutput<MigrateStorageOutput> {
     pub to: ReleaseId,
     pub storage_migration_output: MigrateStorageOutput,
-}
-
-pub fn update_software_and_storage<
-    PackageRelease,
-    ContractError,
-    MigrateStorageFunctor,
-    StorageMigrationOutput,
-    MapErrorFunctor,
->(
-    storage: &mut dyn Storage,
-    previous: PackageRelease,
-    current: PackageRelease,
-    current_release: &PackageRelease::ReleaseId,
-    migrate_storage: MigrateStorageFunctor,
-    map_error: MapErrorFunctor,
-) -> Result<FullUpdateOutput<StorageMigrationOutput>, ContractError>
-where
-    PackageRelease: UpdatablePackage,
-    MigrateStorageFunctor:
-        FnOnce(&mut dyn Storage) -> Result<StorageMigrationOutput, ContractError>,
-    MapErrorFunctor: FnOnce(Error) -> ContractError,
-{
-    previous
-        .update_software_and_storage(&current, current_release)
-        .map_err(map_error)
-        .and_then(|()| {
-            migrate_storage(storage).map(|storage_migration_output| FullUpdateOutput {
-                to: ReleaseId::VOID, //TODO remove the release return value!!!
-                storage_migration_output,
-            })
-        })
 }
