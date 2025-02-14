2298ae8dc3877b312f46afe1f510ca5b04866f08
Katerina Maneva <k.g.maneva@gmail.com>
2025-02-14 12:57:25 +0200

    chore: Add new detailed OverflowErr variant in finance module

--- platform/packages/finance/src/error.rs
+++ platform/packages/finance/src/error.rs
@@ -3,7 +3,7 @@ use std::any::type_name;
 use thiserror::Error;
 
 use currency::error::Error as CurrencyError;
-use sdk::cosmwasm_std::{OverflowError, StdError};
+use sdk::cosmwasm_std::StdError;
 
 use crate::percent::Units as PercentUnits;
 
@@ -12,8 +12,12 @@ pub enum Error {
     #[error("[Finance] Programming error or invalid serialized object of '{0}' type, cause '{1}'")]
     BrokenInvariant(String, String),
 
-    #[error("[Finance] [OverflowError] {0}")]
-    OverflowError(#[from] OverflowError),
+    #[error("[Finance] Overflow {operation}: {operand1} and {operand2}")]
+    OverflowError {
+        operation: String,
+        operand1: String,
+        operand2: String,
+    },
 
     #[error("[Finance] {0}")]
     Overflow(&'static str),
@@ -39,6 +43,18 @@ impl Error {
             Err(Self::BrokenInvariant(type_name::<T>().into(), msg.into()))
         } else {
             Ok(())
+        }
+    }
+
+    pub fn overflow_err(
+        operation: impl ToString,
+        operand1: impl ToString,
+        operand2: impl ToString,
+    ) -> Self {
+        Self::OverflowError {
+            operation: operation.to_string(),
+            operand1: operand1.to_string(),
+            operand2: operand2.to_string(),
         }
     }
 }
