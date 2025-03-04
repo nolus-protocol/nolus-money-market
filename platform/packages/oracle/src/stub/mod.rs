1289fad0e8b47c7fb31a34c98b13385fc2f22012
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2025-03-04 12:42:05 +0200

    chore: Remove commented out code.

--- platform/packages/oracle/src/stub/mod.rs
+++ platform/packages/oracle/src/stub/mod.rs
@@ -95,7 +95,6 @@ where
     where
         V: WithOracle<QuoteC, QuoteG, G = G>,
         G: Group,
-        // QuoteC::Group: MemberOf<G> + MemberOf<G::TopG>,
         QuoteC::Group: MemberOf<G::TopG>,
     {
         cmd.exec(OracleStub::<'_, G, QuoteC, QuoteG, BasePriceRequest>::new(
