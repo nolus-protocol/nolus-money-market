460f2a434be5f6c8eb5ff6e47c47f631de399eb5
Gancho Manev <g.manev@nolus.io>
2025-03-18 16:50:18 +0200

    doc(oracle): make internal links compliant to Markdown

--- protocol/contracts/oracle/src/api/swap.rs
+++ protocol/contracts/oracle/src/api/swap.rs
@@ -21,7 +21,7 @@ where
 {
     /// Provides a path in the swap tree between two arbitrary currencies
     ///
-    /// Returns [Vec<crate::api::SwapTarget>]
+    /// Returns [`Vec<crate::api::SwapTarget>`]
     SwapPath {
         from: CurrencyDTO<PriceCurrencies>,
         to: CurrencyDTO<PriceCurrencies>,
