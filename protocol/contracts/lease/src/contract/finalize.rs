f0e37dde332f366ec3f109c5ef637f99b82729cd
Lockwarr <denislav66@gmail.com>
2025-05-21 10:25:08 +0300

    wip: refactor lease admin checks in leaser

--- protocol/contracts/lease/src/contract/finalize.rs
+++ protocol/contracts/lease/src/contract/finalize.rs
@@ -1,4 +1,3 @@
-use access_control::GrantedAddress;
 use serde::{Deserialize, Serialize};
 
 use platform::{batch::Batch, contract::Validator};
@@ -7,6 +6,7 @@ use sdk::cosmwasm_std::{Addr, QuerierWrapper};
 use crate::{
     api::{
         FinalizerExecuteMsg,
+        authz::{AccessCheck, AccessGranted},
         limits::{MaxSlippage, PositionLimits},
     },
     error::{ContractError, ContractResult},
@@ -49,8 +49,17 @@ impl LeasesRef {
     pub(super) fn check_access(
         &self,
         caller: Addr,
+        querier: QuerierWrapper<'_>,
     ) -> ContractResult<()> {
-        access_control::check(&GrantedAddress::new(&self.addr), &caller)
-        .map_err(ContractError::Unauthorized)
+        let query = AccessCheck::AnomalyResolution { by: caller };
+        querier
+            .query_wasm_smart(self.addr.clone(), &query)
+            .map_err(ContractError::CheckAccessQuery)
+            .and_then(|access: AccessGranted| match access {
+                AccessGranted::No => Err(ContractError::Unauthorized(
+                    access_control::error::Error::Unauthorized {},
+                )),
+                AccessGranted::Yes => Ok(()),
+            })
     }
 }
