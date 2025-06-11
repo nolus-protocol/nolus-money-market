a6f9448ad12dea678534fb5b648db81306e1d491
desislavva <dessislv28th@gmail.com>
2025-06-11 11:12:58 +0300

    refactor(scripts): LPP init msg - remove lpn ticker

--- scripts/deploy-protocol.sh
+++ scripts/deploy-protocol.sh
@@ -177,7 +177,7 @@ deploy_contracts() {
     local -r lease_code_id=$(store_code "$nolus_net" "$nolus_home_dir" "$store_code_privileged_wallet_key" "$wasm_path/lease.wasm" "--instantiate-anyof-addresses $leaser_expected_address")
 
     # upload and instantiate LPP
-    local -r lpp_init_msg='{"lpn_ticker":"'"$protocol_currency"'","lease_code_admin":"'"$leaser_expected_address"'","lease_code":"'"$lease_code_id"'","borrow_rate":{"base_interest_rate":60,"utilization_optimal":700,"addon_optimal_interest_rate":20},"min_utilization":0}'
+    local -r lpp_init_msg='{"lease_code_admin":"'"$leaser_expected_address"'","lease_code":"'"$lease_code_id"'","borrow_rate":{"base_interest_rate":60,"utilization_optimal":700,"addon_optimal_interest_rate":20},"min_utilization":0}'
     local -r lpp_contract_address=$(_deploy_contract "$nolus_net" "$nolus_home_dir" "$dex_admin_wallet_key" "$store_code_privileged_wallet_key" "$admin_contract_address" "$wasm_path/lpp.wasm" "$lpp_init_msg" "$protocol-lpp" "$protocol")
 
     # upload and instantiate Oracle
