f48a9bbc40da98f0c625209e900085a8388fc1f8
Lockwarr <denislav66@gmail.com>
2025-06-12 13:16:22 +0300

    chore: address comments regarding naming and modules

--- platform/packages/access-control/src/permissions.rs
+++ platform/packages/access-control/src/permissions.rs
@@ -4,6 +4,8 @@ use crate::{AccessPermission, user::User};
 
 pub type DexResponseSafeDeliveryPermission<'a> = SameContractOnly<'a>;
 
+pub type DexResponseSafeDeliveryPermission<'a> = SameContractOnly<'a>;
+
 pub struct SingleUserPermission<'a> {
     addr: &'a Addr,
 }
--- protocol/contracts/leaser/src/contract.rs
+++ protocol/contracts/leaser/src/contract.rs
@@ -52,7 +52,7 @@ impl<'a> LeaseAdminOnly<'a> {
 }
 
 impl AccessPermission for LeaseAdminOnly<'_> {
-    fn is_granted_to(&self, info: &MessageInfo) -> bool {
+    fn granted_to(&self, info: &MessageInfo) -> bool {
         self.lease_config.lease_admin == info.sender
     }
 }
