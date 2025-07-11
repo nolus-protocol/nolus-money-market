d29f6d65cf8573defe6563a2203ba5931d783fdf
Katerina Maneva <k.g.maneva@gmail.com>
2025-07-11 14:52:04 +0300

    refactor(finance): Create Percent100 for cases when the Percent has to be no more than 100%

--- platform/packages/finance/src/fractionable/price.rs
+++ platform/packages/finance/src/fractionable/price.rs
@@ -1,17 +1,23 @@
 use std::marker::PhantomData;
 
-use crate::{coin::Amount, percent::Units as PercentUnits, price::Price, ratio::Ratio};
+use crate::{
+    coin::Amount,
+    percent::{Units as PercentUnits, bound::BoundPercent},
+    price::Price,
+    ratio::Ratio,
+};
 
 use super::Fractionable;
 
-impl<C, QuoteC> Fractionable<PercentUnits> for Price<C, QuoteC>
+impl<const UPPER_BOUND: PercentUnits, C, QuoteC> Fractionable<BoundPercent<UPPER_BOUND>>
+    for Price<C, QuoteC>
 where
     C: 'static,
     QuoteC: 'static,
 {
     fn safe_mul<F>(self, fraction: &F) -> Self
     where
-        F: Ratio<PercentUnits>,
+        F: Ratio<BoundPercent<UPPER_BOUND>>,
     {
         self.lossy_mul(&RatioUpcast(PhantomData, fraction))
     }
@@ -73,19 +79,19 @@ mod test {
     mod percent {
         use crate::fraction::Fraction;
         use crate::fractionable::price::test::{c, q};
-        use crate::{percent::Percent, price};
+        use crate::{percent::Percent100, price};
 
         #[test]
         fn greater_than_one() {
             let price = price::total_of(c(1)).is(q(1000));
-            let permille = Percent::from_permille(1);
+            let permille = Percent100::from_permille(1);
             assert_eq!(permille.of(price), price::total_of(c(1)).is(q(1)));
         }
 
         #[test]
         fn less_than_one() {
             let price = price::total_of(c(10)).is(q(1));
-            let twenty_percents = Percent::from_percent(20);
+            let twenty_percents = Percent100::from_percent(20);
             assert_eq!(twenty_percents.of(price), price::total_of(c(50)).is(q(1)));
         }
     }
@@ -94,10 +100,10 @@ mod test {
 
         use crate::{
             coin::{Amount, Coin},
-            fraction::Fraction,
             fractionable::price::test::{c, q},
             price,
             ratio::SimpleFraction,
+            rational::Rational,
         };
 
         #[test]
@@ -145,7 +151,7 @@ mod test {
             let price = price::total_of(amount1).is(quote1);
             let ratio = SimpleFraction::new(nominator, denominator);
             assert_eq!(
-                Fraction::<usize>::of(&ratio, price),
+                ratio.of(price).unwrap(),
                 price::total_of(amount_exp).is(quote_exp)
             );
         }
--- platform/packages/finance/src/percent/bound.rs
+++ platform/packages/finance/src/percent/bound.rs
@@ -1,57 +1,185 @@
+use std::{
+    fmt::{Debug, Display, Formatter, Result as FmtResult, Write},
+    num::TryFromIntError,
+    ops::Div,
+};
+
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    error::{Error, Result},
+    coin::Amount,
+    error::{Error, Result as FinanceResult},
+    ratio::{Ratio, SimpleFraction},
+    traits::FractionUnit,
     zero::Zero,
 };
 
-use super::{Percent, Units};
+use super::{HUNDRED_BOUND, Units};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default, Serialize, Deserialize)]
-#[serde(into = "Percent", try_from = "Percent")]
-pub struct BoundPercent<const UPPER_BOUND: Units>(Percent);
+#[derive(Clone, Copy, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+#[serde(try_from = "Units", into = "Units")]
+pub struct BoundPercent<const UPPER_BOUND: Units>(Units);
 
 impl<const UPPER_BOUND: Units> BoundPercent<UPPER_BOUND> {
-    pub const ZERO: Self = Self(Percent::ZERO);
+    pub const ZERO: Self = Self::from_permille(0);
+    pub const HUNDRED: Self = Self::from_permille(Self::PERMILLE);
+    pub(crate) const PERMILLE: Units = HUNDRED_BOUND;
 
-    pub const fn try_from_percent(percent: Percent) -> Result<Self> {
-        if percent.units() <= UPPER_BOUND {
-            Ok(Self(percent))
-        } else {
-            Err(Error::UpperBoundCrossed {
-                bound: UPPER_BOUND,
-                value: percent.units(),
-            })
-        }
+    const UNITS_TO_PERCENT_RATIO: Units = 10;
+
+    #[cfg(any(test, feature = "testing"))]
+    pub fn new(units: Units) -> Self {
+        Self::new_internal(units)
     }
 
-    #[cfg(feature = "testing")]
-    pub fn strict_from_percent(percent: Percent) -> Self {
-        Self::try_from_percent(percent)
-            .expect("BoundPercent created with a value within the limits")
+    const fn new_internal(units: Units) -> Self {
+        debug_assert!(units <= UPPER_BOUND, "Value exceeds upper bound!");
+        Self(units)
     }
 
-    pub const fn percent(&self) -> Percent {
+    pub fn from_percent(percent: u16) -> Self {
+        Self::from_permille(Units::from(percent) * Self::UNITS_TO_PERCENT_RATIO)
+    }
+
+    pub const fn from_permille(permille: Units) -> Self {
+        Self::new_internal(permille)
+    }
+
+    // pub fn from_ratio<U>(nominator: U, denominator: U) -> Option<Self>
+    // where
+    //     Self: Fractionable<U>,
+    //     U: FractionUnit,
+    // {
+    //     SimpleFraction::new(nominator, denominator).lossy_mul(Self::HUNDRED)
+    // }
+
+    pub const fn units(&self) -> Units {
         self.0
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self == &Self::ZERO
+    }
+
+    pub fn checked_add(self, other: Self) -> FinanceResult<Self> {
+        self.0
+            .checked_add(other.0)
+            .ok_or(Error::overflow_err("while adding", self, other))
+            .and_then(|sum| {
+                if sum <= UPPER_BOUND {
+                    Ok(Self::from_permille(sum))
+                } else {
+                    Err(Error::UpperBoundCrossed {
+                        bound: HUNDRED_BOUND,
+                        value: sum,
+                    })
+                }
+            })
+    }
+
+    pub fn checked_sub(self, other: Self) -> FinanceResult<Self> {
+        self.0
+            .checked_sub(other.0)
+            .map(Self::from_permille)
+            .ok_or(Error::overflow_err("while subtracting", self, other))
+    }
+}
+
+// Method used for deserialization
+impl<const UPPER_BOUND: Units> TryFrom<Units> for BoundPercent<UPPER_BOUND> {
+    type Error = Error;
+
+    fn try_from(permille: Units) -> Result<Self, Self::Error> {
+        (permille <= UPPER_BOUND)
+            .then(|| Self::new_internal(permille))
+            .ok_or(Error::UpperBoundCrossed {
+                bound: UPPER_BOUND,
+                value: permille,
+            })
+    }
+}
+
+// Method used for serialization
+impl<const UPPER_BOUND: Units> From<BoundPercent<UPPER_BOUND>> for Units {
+    fn from(percent: BoundPercent<UPPER_BOUND>) -> Self {
+        percent.0
+    }
+}
+
+impl<const UPPER_BOUND: Units> Display for BoundPercent<UPPER_BOUND> {
+    #[track_caller]
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        let whole = (self.0) / Self::UNITS_TO_PERCENT_RATIO;
+        let (no_fraction, overflow) = whole.overflowing_mul(Self::UNITS_TO_PERCENT_RATIO);
+        debug_assert!(!overflow);
+        let (fractional, overflow) = (self.0).overflowing_sub(no_fraction);
+        debug_assert!(!overflow);
+
+        f.write_fmt(format_args!("{whole}"))?;
+        if fractional != Units::default() {
+            f.write_fmt(format_args!(".{fractional}"))?;
+        }
+        f.write_char('%')?;
+        Ok(())
+    }
+}
+
+impl<const UPPER_BOUND: Units> Div for BoundPercent<UPPER_BOUND> {
+    type Output = Units;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        debug_assert!(!rhs.is_zero());
+
+        self.0.div(rhs.0)
+    }
+}
+
+impl<const UPPER: Units> Ratio<Units> for BoundPercent<UPPER> {
+    fn parts(&self) -> Units {
+        self.units()
+    }
+
+    fn total(&self) -> Units {
+        Self::HUNDRED.units()
+    }
+}
+
+impl<const UPPER: Units> FractionUnit for BoundPercent<UPPER> where
+    BoundPercent<UPPER>: Copy + Debug + Ord + Zero
+{
+}
+
+impl<const UPPER_BOUND: Units> From<BoundPercent<UPPER_BOUND>> for Amount {
+    fn from(percent: BoundPercent<UPPER_BOUND>) -> Self {
+        Amount::from(percent.units())
+    }
+}
+
+impl<const UPPER_BOUND: Units> From<BoundPercent<UPPER_BOUND>> for SimpleFraction<Amount> {
+    fn from(percent: BoundPercent<UPPER_BOUND>) -> Self {
+        Self::new(
+            percent.0.into(),
+            BoundPercent::<UPPER_BOUND>::HUNDRED.0.into(),
+        )
+    }
+}
+
+impl<const UPPER_BOUND: Units> TryFrom<Amount> for BoundPercent<UPPER_BOUND> {
+    type Error = <Units as TryFrom<Amount>>::Error;
+
+    fn try_from(value: Amount) -> Result<Self, Self::Error> {
+        Ok(Self::from_permille(value.try_into()?))
+    }
+}
+
+impl<const UPPER_BOUND: Units> TryFrom<BoundPercent<UPPER_BOUND>> for u16 {
+    type Error = TryFromIntError;
+
+    fn try_from(percent: BoundPercent<UPPER_BOUND>) -> Result<Self, Self::Error> {
+        percent.0.try_into()
     }
 }
 
 impl<const UPPER_BOUND: Units> Zero for BoundPercent<UPPER_BOUND> {
     const ZERO: Self = Self::ZERO;
 }
-
-impl<const UPPER_BOUND: Units> TryFrom<Percent> for BoundPercent<UPPER_BOUND> {
-    type Error = Error;
-
-    fn try_from(value: Percent) -> Result<Self> {
-        Self::try_from_percent(value)
-    }
-}
-
-impl<const UPPER_BOUND: Units> From<BoundPercent<UPPER_BOUND>> for Percent {
-    fn from(value: BoundPercent<UPPER_BOUND>) -> Self {
-        value.percent()
-    }
-}
-
-pub type BoundToHundredPercent = BoundPercent<{ Percent::HUNDRED.units() }>;
