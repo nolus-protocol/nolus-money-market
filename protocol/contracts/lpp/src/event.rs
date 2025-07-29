b00761a61f73ec3125cf12805bdc14b71283560c
Gancho Manev <g.manev@nolus.io>
2025-07-29 16:00:04 +0300

    feat(lpp): add WithdrawEmitter

--- protocol/contracts/lpp/src/event.rs
+++ protocol/contracts/lpp/src/event.rs
@@ -1,4 +1,6 @@
-use currency::CurrencyDef;
+use std::marker::PhantomData;
+
+use currency::{CurrencyDef, platform::Nls};
 use finance::coin::Coin;
 use lpp_platform::NLpn;
 use platform::batch::{Emit, Emitter};
@@ -21,21 +23,50 @@ where
         .emit_coin_amount("receipts", receipts)
 }
 
-pub fn emit_withdraw<Lpn>(
-    env: Env,
-    lender_addr: Addr,
-    payment_lpn: Coin<Lpn>,
-    receipts: Coin<NLpn>,
-    close_flag: bool,
-) -> Emitter
+/// An events emitter supporting single and multiple withdrawals
+pub(crate) struct WithdrawEmitter<'env, Lpn> {
+    env: &'env Env,
+    events: Vec<Emitter>,
+    _lpn: PhantomData<Lpn>,
+}
+
+impl<'env, Lpn> WithdrawEmitter<'env, Lpn>
 where
     Lpn: CurrencyDef,
 {
-    Emitter::of_type("lp-withdraw")
-        .emit_tx_info(&env)
-        .emit("to", lender_addr)
-        .emit("from", env.contract.address)
-        .emit_coin("withdraw", payment_lpn)
-        .emit_coin_amount("receipts", receipts)
-        .emit_to_string_value("close", close_flag)
+    pub fn new(env: &'env Env) -> Self {
+        Self {
+            env,
+            events: Default::default(),
+            _lpn: Default::default(),
+        }
+    }
+
+    pub fn on_withdraw(
+        &mut self,
+        lender: Addr,
+        receipts: Coin<NLpn>,
+        payment_out: Coin<Lpn>,
+        may_reward: Option<Coin<Nls>>,
+    ) {
+        self.events.push(
+            Emitter::of_type("lp-withdraw")
+                .emit_tx_info(self.env)
+                .emit("to", lender)
+                .emit("from", self.env.contract.address.clone())
+                .emit_coin("withdraw", payment_out)
+                .emit_coin_amount("receipts", receipts)
+                .emit_to_string_value("close", may_reward.is_some()),
+        );
+    }
+}
+
+impl<Lpn> IntoIterator for WithdrawEmitter<'_, Lpn> {
+    type Item = Emitter;
+
+    type IntoIter = <Vec<Self::Item> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.events.into_iter()
+    }
 }
