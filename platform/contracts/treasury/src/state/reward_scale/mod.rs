b235db78b281eeb862f06001a2507892bac85566
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2025-08-04 12:59:29 +0300

    refactor(finance): remove From<Amount> for Coin
    Using From trait is obsolete since it calls Coin::new. All occurrences are replaced directly with the ::new function

--- platform/contracts/treasury/src/state/reward_scale/mod.rs
+++ platform/contracts/treasury/src/state/reward_scale/mod.rs
@@ -26,8 +26,8 @@ impl TotalValueLocked {
     {
         Amount::from(self.0)
             .checked_mul(Self::SCALE_FACTOR)
+            .map(|amount| Coin::new(amount))
             .expect("Amount goes beyond calculation limits!")
-            .into()
     }
 }
 
@@ -188,7 +188,7 @@ mod tests {
         .unwrap();
 
         assert_eq!(
-            res.get_apr::<SuperGroupTestC1>(0.into()),
+            res.get_apr::<SuperGroupTestC1>(Coin::new(0)),
             Percent100::from_permille(6)
         );
         assert_eq!(
@@ -243,6 +243,6 @@ mod tests {
     }
 
     fn coin(amount: Amount) -> Coin<SuperGroupTestC1> {
-        amount.into()
+        Coin::new(amount)
     }
 }
