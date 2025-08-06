6d50f4902c631c5a1f73602ef2d3c2f293d96c93
Katerina Maneva <k.g.maneva@gmail.com>
2025-08-06 10:55:56 +0300

    chore: Fix imports

--- protocol/contracts/lpp/src/state/total.rs
+++ protocol/contracts/lpp/src/state/total.rs
@@ -35,7 +35,8 @@ pub struct Total<Lpn> {
 pub mod migrate_from_0_8_12 {
     use serde::{Deserialize, Serialize, Serializer};
 
-    use finance::{coin::Coin, ratio::Rational};
+    use cosmwasm_std::{Storage, Timestamp};
+    use finance::{coin::Coin, ratio::SimpleFraction};
     use lpp_platform::NLpn;
     use sdk::{
         cosmwasm_std::{Storage, Timestamp},
@@ -50,7 +51,7 @@ pub mod migrate_from_0_8_12 {
         pub struct Total0_8_12<Lpn> {
             total_principal_due: Coin<Lpn>,
             total_interest_due: Coin<Lpn>,
-            annual_interest_rate: Rational<Coin<Lpn>>,
+            annual_interest_rate: SimpleFraction<Coin<Lpn>>,
             last_update_time: Timestamp,
         }
         impl<Lpn> Serialize for Total0_8_12<Lpn> {
