f343a08f95f5c1827ac8863ad04cd8bd337b983b
Katerina Maneva <k.g.maneva@gmail.com>
2025-08-06 09:57:17 +0300

    refactor(finance): Rename FractionUnit to Unit and move it to fraction module

--- platform/packages/finance/src/fraction.rs
+++ platform/packages/finance/src/fraction.rs
@@ -1,7 +1,15 @@
-use crate::fractionable::Fractionable;
+use std::fmt::Debug;
+
+use crate::{fractionable::Fractionable, zero::Zero};
 
 pub trait Fraction<U> {
     fn of<A>(self, whole: A) -> A
     where
         A: Fractionable<U>;
+}
+
+pub trait Unit
+where
+    Self: Copy + Debug + Ord + Zero,
+{
 }
--- platform/packages/finance/src/traits.rs
+++
@@ -1,9 +0,0 @@
-use std::fmt::Debug;
-
-use crate::zero::Zero;
-
-pub trait FractionUnit
-where
-    Self: Copy + Debug + Ord + Zero,
-{
-}
