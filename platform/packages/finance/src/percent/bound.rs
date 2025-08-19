97a87c17478617636b8d380eb084ddedb8bada4d
Katerina Maneva <k.g.maneva@gmail.com>
2025-08-19 10:26:32 +0300

    chore(finance): Add and move tests

--- platform/packages/finance/src/percent/bound.rs
+++ platform/packages/finance/src/percent/bound.rs
@@ -178,7 +178,14 @@ impl<const UPPER_BOUND: Units> Sub for BoundPercent<UPPER_BOUND> {
 
 #[cfg(test)]
 mod test {
-    use crate::percent::{Percent, Percent100};
+    use currency::test::SubGroupTestC10;
+
+    use crate::{
+        coin::Coin,
+        fraction::Fraction,
+        percent::{Percent, Percent100, Units},
+        rational::Rational,
+    };
 
     #[test]
     fn from_primitive() {
@@ -222,5 +229,80 @@ mod test {
             Percent::from_permille(1001)
         );
         assert!(Percent::try_from_primitive(u32::MAX / 10 + 1).is_none());
+    }
+
+    #[test]
+    fn test_zero() {
+        let zero_amount = Coin::<SubGroupTestC10>::new(0);
+        assert_eq!(
+            zero_amount,
+            Percent100::ZERO.of(Coin::<SubGroupTestC10>::new(10))
+        );
+        assert_eq!(
+            zero_amount,
+            Percent::ZERO.of(Coin::<SubGroupTestC10>::new(10)).unwrap()
+        )
+    }
+
+    #[test]
+    fn test_hundred() {
+        let amount = Coin::<SubGroupTestC10>::new(123);
+        assert_eq!(amount, Percent100::HUNDRED.of(amount));
+        assert_eq!(amount, Percent::HUNDRED.of(amount).unwrap())
+    }
+
+    #[test]
+    fn checked_add() {
+        assert_eq!(from(40), from(25) + (from(15)));
+        assert_eq!(from(39), from(0) + (from(39)));
+        assert_eq!(from(39), from(39) + (from(0)));
+        assert_eq!(Percent100::HUNDRED, from(999) + (from(1)));
+    }
+
+    #[test]
+    fn add_overflow() {
+        assert!(Percent100::HUNDRED.checked_add(from(1)).is_none());
+        assert!(
+            Percent::from_permille(Units::MAX)
+                .checked_add(Percent::from_permille(1))
+                .is_none()
+        );
+    }
+
+    #[test]
+    fn sub() {
+        assert_eq!(from(67), from(79) - (from(12)));
+        assert_eq!(from(0), from(34) - (from(34)));
+        assert_eq!(from(39), from(39) - (from(0)));
+        assert_eq!(from(990), Percent100::HUNDRED - (from(10)));
+        assert_eq!(
+            from(0),
+            Percent100::HUNDRED - Percent100::from_permille(1000)
+        );
+    }
+
+    #[test]
+    fn sub_overflow() {
+        assert!(from(34).checked_sub(from(35)).is_none())
+    }
+
+    #[test]
+    fn display() {
+        test_display("0%", 0);
+        test_display("0.1%", 1);
+        test_display("0.4%", 4);
+        test_display("1%", 10);
+        test_display("1.9%", 19);
+        test_display("9%", 90);
+        test_display("10.1%", 101);
+        test_display("100%", 1000);
+    }
+
+    fn from(permille: Units) -> Percent100 {
+        Percent100::from_permille(permille)
+    }
+
+    fn test_display(exp: &str, permilles: Units) {
+        assert_eq!(exp, format!("{}", Percent100::from_permille(permilles)));
     }
 }
