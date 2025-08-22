772a3f4daaf697dc2a51f29a411bc30fd2a7c4b9
Katerina Maneva <k.g.maneva@gmail.com>
2025-08-22 14:59:23 +0300

    refactor: PositionSpec::ltv() returns Percent100

--- protocol/contracts/lease/src/position/spec/mod.rs
+++ protocol/contracts/lease/src/position/spec/mod.rs
@@ -7,7 +7,7 @@ use finance::{
     fraction::{Fraction, Unit as FractionUnit},
     fractionable::Fractionable,
     liability::{Liability, Zone},
-    percent::Percent,
+    percent::{Percent, Percent100},
     price::{self},
     zero::Zero,
 };
@@ -395,12 +395,16 @@ impl Spec {
         due.overdue_collection(self.min_transaction)
     }
 
-    fn ltv<P>(total_due: P, lease_asset: P) -> Percent
+    fn ltv<P>(total_due: P, lease_asset: P) -> Percent100
     where
         P: Copy + Debug + FractionUnit + PartialEq + Zero,
-        Percent: Fractionable<P>,
+        Percent100: Fractionable<P>,
     {
-        Percent::from_fraction(total_due, lease_asset).expect("Expect having a ltv")
+        debug_assert!(total_due <= lease_asset);
+
+        Percent100::from_fraction(total_due, lease_asset).expect(
+            "TODO: The ltv has to be at most 100%. Remove the expect once Ratio become a struct",
+        )
     }
 
     fn zone<Asset>(&self, asset: Coin<Asset>, due_asset: Coin<Asset>) -> Zone
