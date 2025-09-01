9b718e8600f764200a798b79a2ccb3b3a665876e
Katerina Maneva <k.g.maneva@gmail.com>
2025-09-01 14:19:58 +0300

    refactor(finance): Ad an invariant check for Ratio and fix tests

--- platform/packages/finance/src/ratio.rs
+++ platform/packages/finance/src/ratio.rs
@@ -12,11 +12,7 @@ use crate::{
 // /// A part of something that is divisible
 #[derive(Clone, Deserialize, Serialize)]
 #[cfg_attr(any(test, feature = "testing"), derive(Debug, PartialEq))]
-#[serde(
-    try_from = "Rational<U>",
-    into = "Rational<U>",
-    rename_all = "snake_case"
-)]
+#[serde(try_from = "Rational<U>", into = "Rational<U>")]
 pub struct Ratio<U>(Rational<U>)
 where
     U: Clone + Debug + PartialOrd + Zero;
@@ -25,9 +21,17 @@ impl<U> Ratio<U>
 where
     U: Clone + Debug + PartialOrd + Zero,
 {
-    pub(crate) fn new(parts: U, total: U) -> FinanceResult<Self> {
-        Error::broken_invariant_if::<Self>(parts > total, "Parts must not exceed total")
-            .map(|()| Self(Rational::new(parts, total)))
+    pub(crate) fn new(parts: U, total: U) -> Self {
+        let obj = Self(Rational::new(parts, total));
+        debug_assert_eq!(Ok(()), obj.invariant_held());
+        obj
+    }
+
+    fn invariant_held(&self) -> FinanceResult<()> {
+        Error::broken_invariant_if::<Self>(
+            self.0.nominator > self.0.denominator,
+            "Parts must not exceed total",
+        )
     }
 }
 
@@ -38,7 +42,9 @@ where
     type Error = Error;
 
     fn try_from(rational: Rational<U>) -> Result<Self, Self::Error> {
-        Self::new(rational.nominator, rational.denominator)
+        let res = Self::new(rational.nominator, rational.denominator);
+        res.invariant_held()?;
+        Ok(res)
     }
 }
 
@@ -107,10 +113,11 @@ where
 
 #[cfg(test)]
 mod test_ratio {
-    use std::fmt::{Debug, Display};
+    use std::fmt::Debug;
 
     use currency::test::SuperGroupTestC1;
     use sdk::cosmwasm_std;
+    use serde::{Serialize, de::DeserializeOwned};
 
     use crate::{
         coin::{Amount, Coin},
@@ -119,82 +126,64 @@ mod test_ratio {
     };
 
     #[test]
-    fn ratio_new() {
-        assert!(Ratio::new(1u32, 2u32).is_ok());
-        assert!(Ratio::new(coin(9), coin(10)).is_ok());
-        assert!(Ratio::new(coin(10), coin(10)).is_ok());
+    fn valid_invariant() {
+        assert!(Ratio::new(1u32, 2u32).invariant_held().is_ok());
+        assert!(Ratio::new(coin(9), coin(10)).invariant_held().is_ok());
+        assert!(Ratio::new(coin(10), coin(10)).invariant_held().is_ok());
     }
 
     #[test]
-    fn ratio_new_invalid() {
-        let msg = "Parts must not exceed total";
-        assert_err(Ratio::new(4u32, 3u32), msg);
-        assert_err(Ratio::new(coin(10), coin(9)), msg);
+    #[should_panic = "Parts must not exceed total"]
+    fn invalid_variant() {
+        let _ = Ratio::new(4u32, 3u32);
+        let _ = Ratio::new(coin(10), coin(9));
     }
 
     #[test]
     fn serialize() {
         assert_eq!(
             r#"{"nominator":5,"denominator":7}"#,
-            cosmwasm_std::to_json_string(&Ratio::new(5u32, 7u32).unwrap()).unwrap()
+            cosmwasm_std::to_json_string(&Ratio::new(5u32, 7u32)).unwrap()
         );
         assert_eq!(
             r#"{"nominator":{"amount":"5"},"denominator":{"amount":"7"}}"#,
-            cosmwasm_std::to_json_string(&Ratio::new(coin(5), coin(7)).unwrap()).unwrap()
+            cosmwasm_std::to_json_string(&Ratio::new(coin(5), coin(7))).unwrap()
         );
     }
 
     #[test]
     fn deserialize() {
         assert_eq!(
-            Ratio::new(coin(5), coin(5)).unwrap(),
+            Ratio::new(coin(5), coin(5)),
             cosmwasm_std::from_json(r#"{"nominator":{"amount":"5"},"denominator":{"amount":"5"}}"#)
                 .unwrap()
         );
     }
 
     #[test]
-    fn serialize_deserialize() {
-        let serialized_1 = cosmwasm_std::to_json_vec(&Rational::new(coin(3), coin(4))).unwrap();
-        assert_eq!(
-            Ratio::new(coin(3), coin(4)).unwrap(),
-            cosmwasm_std::from_json(&serialized_1).unwrap()
-        );
-
-        let serialized_2 =
-            cosmwasm_std::to_json_vec(&Rational::new(coin(Amount::MAX), coin(Amount::MAX)))
-                .unwrap();
-        assert_eq!(
-            Ratio::new(coin(Amount::MAX), coin(Amount::MAX)).unwrap(),
-            cosmwasm_std::from_json(&serialized_2).unwrap()
-        );
+    fn serialize_deserialize_test() {
+        serialize_deserialize(Ratio::new(coin(3), coin(4)));
+        serialize_deserialize(Ratio::new(coin(Amount::MAX), coin(Amount::MAX)));
     }
 
     #[test]
+    #[should_panic = "Parts must not exceed total"]
     fn serialize_deserialize_invalid() {
         let serialized = cosmwasm_std::to_json_vec(&Rational::new(coin(5), coin(4))).unwrap();
-
-        assert_err::<Coin<SuperGroupTestC1>, _>(
-            cosmwasm_std::from_json(&serialized),
-            "Parts must not exceed total",
-        );
+        let _ = cosmwasm_std::from_json::<Ratio<Coin<SuperGroupTestC1>>>(&serialized);
     }
 
     const fn coin(amount: Amount) -> Coin<SuperGroupTestC1> {
         Coin::new(amount)
     }
 
-    fn assert_err<U, E>(res: Result<Ratio<U>, E>, msg: &str)
+    fn serialize_deserialize<U>(ratio: Ratio<U>)
     where
-        U: Clone + Debug + PartialOrd + Zero,
-        E: Display,
+        U: Clone + Debug + DeserializeOwned + PartialOrd + Serialize + Zero,
     {
-        let err = res.unwrap_err();
-        assert!(
-            err.to_string().contains(msg),
-            "Error `{}` does not contain expected `{}`",
-            err,
-            msg
-        );
+        let serialized = cosmwasm_std::to_json_vec(&ratio).unwrap();
+        let deserialized = cosmwasm_std::from_json::<Ratio<U>>(&serialized).unwrap();
+
+        assert_eq!(ratio, deserialized)
     }
 }
