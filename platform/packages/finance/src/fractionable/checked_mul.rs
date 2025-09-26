e595bf67dcb14ebc9cef68e45748c29eaf4aaa7a
Katerina Maneva <k.g.maneva@gmail.com>
2025-09-26 09:18:19 +0300

    chore(finance): Define the traits that will replace the FractionableLegacy<Coin<C>> for Duration

--- platform/packages/finance/src/fractionable/checked_mul.rs
+++ platform/packages/finance/src/fractionable/checked_mul.rs
@@ -1,5 +1,15 @@
+use bnum::types::U256;
+
 pub trait CheckedMul<Rhs = Self> {
     type Output;
 
     fn checked_mul(self, rhs: Rhs) -> Option<Self::Output>;
+}
+
+impl CheckedMul for U256 {
+    type Output = Self;
+
+    fn checked_mul(self, rhs: Self) -> Option<Self::Output> {
+        self.checked_mul(rhs)
+    }
 }
