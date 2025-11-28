639bf9bbbc4c252551ff9a4e7bc74434d3d17b31
Katerina Maneva <k.g.maneva@gmail.com>
2025-11-28 16:07:17 +0200

    chore(lpp): Add doc for deposit_capacity()

--- protocol/contracts/lpp/src/lpp.rs
+++ protocol/contracts/lpp/src/lpp.rs
@@ -78,6 +78,11 @@ where
     Lpn: CurrencyDef,
     Bank: BankAccountView,
 {
+    /// Computes the remaining deposit capacity under the minimum utilization constraint.
+    /// Returns:
+    /// - `Ok(None)` -> no minimum utilization constraint is set
+    /// - `Ok(Some(amount))` -> the maximum additional deposit allowed (may be zero)
+    /// - `Err(_)` -> an error occured while computing the commited balance
     pub fn deposit_capacity(
         &self,
         now: &Timestamp,
