d7d51ee24cb6b013b601c6f8909c3b2275108c60
Katerina Maneva <k.g.maneva@gmail.com>
2025-12-02 10:56:09 +0200

    refactor(finance): Remove DoublePercentPrimitive type

--- platform/packages/finance/src/fractionable/checked_mul.rs
+++ platform/packages/finance/src/fractionable/checked_mul.rs
@@ -1,4 +1,4 @@
-use crate::{coin::DoubleCoinPrimitive, percent::DoublePercentPrimitive};
+use crate::coin::DoubleCoinPrimitive;
 
 pub trait CheckedMul<Rhs = Self> {
     type Output;
@@ -6,7 +6,7 @@ pub trait CheckedMul<Rhs = Self> {
     fn checked_mul(self, rhs: Rhs) -> Option<Self::Output>;
 }
 
-impl CheckedMul for DoublePercentPrimitive {
+impl CheckedMul for u64 {
     type Output = Self;
 
     fn checked_mul(self, rhs: Self) -> Option<Self::Output> {
