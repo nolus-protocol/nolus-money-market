21b4a848aed421f8da0e4525ab4e13e037ddf921
Katerina Maneva <k.g.maneva@gmail.com>
2025-12-16 15:30:59 +0200

    chore(finance): Replace U256 with DoubleCoinPrimitive

--- platform/packages/finance/src/coin/amount.rs
+++ platform/packages/finance/src/coin/amount.rs
@@ -1,29 +1,27 @@
-use bnum::types::U256;
-
 use crate::{
-    coin::Amount,
+    coin::{Amount, DoubleCoinPrimitive},
     fractionable::{IntoMax, ToDoublePrimitive, TryFromMax},
 };
 
 // TODO remove this implemenatation when Price converts to SimpleFraction<Quote, C>
-impl IntoMax<U256> for Amount {
-    fn into_max(self) -> U256 {
+impl IntoMax<DoubleCoinPrimitive> for Amount {
+    fn into_max(self) -> DoubleCoinPrimitive {
         self.to_double()
     }
 }
 
 // TODO remove this implemenatation when Price converts to SimpleFraction<Quote, C>
 impl ToDoublePrimitive for Amount {
-    type Double = U256;
+    type Double = DoubleCoinPrimitive;
 
     fn to_double(&self) -> Self::Double {
-        U256::from(*self)
+        DoubleCoinPrimitive::from(*self)
     }
 }
 
 // TODO remove this implemenatation when Price converts to SimpleFraction<Quote, C>
-impl TryFromMax<U256> for Amount {
-    fn try_from_max(max: U256) -> Option<Self> {
+impl TryFromMax<DoubleCoinPrimitive> for Amount {
+    fn try_from_max(max: DoubleCoinPrimitive) -> Option<Self> {
         max.try_into().ok()
     }
 }
