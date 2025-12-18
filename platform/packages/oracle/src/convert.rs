d3b9c2ceb071a4589fe8b0d861019880082e4b56
Katerina Maneva <k.g.maneva@gmail.com>
2025-12-18 17:42:44 +0200

    chore(oracle): Move tests from oracle::test.rs to oracle::convert.rs

--- platform/packages/oracle/src/convert.rs
+++ platform/packages/oracle/src/convert.rs
@@ -110,3 +110,63 @@ mod impl_ {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use currency::{platform::Stable, test::SuperGroupTestC1};
+    use finance::coin::{Amount, Coin};
+
+    use crate::test::DummyOracle;
+
+    #[test]
+    fn from_quote() {
+        assert_from_quote(3, 12, 4);
+        assert_from_quote(1, 4, 4);
+        assert_from_quote(4, 4, 1);
+        assert_from_quote(2, 14, 7);
+        assert_from_quote(10, 9, 0);
+        assert_from_quote(2, Amount::MAX, Amount::MAX / 2);
+        assert_from_quote(Amount::MAX / 5, 4, 20 / Amount::MAX);
+        assert_from_quote(Amount::MAX, 5, 0);
+        assert_from_quote(Amount::MAX, Amount::MAX, 1);
+    }
+
+    #[test]
+    fn to_quote() {
+        assert_to_quote(4, 3, 12);
+        assert_to_quote(1, 6, 6);
+        assert_to_quote(10, 4, 40);
+        assert_to_quote(7, 1, 7);
+        assert_to_quote(Amount::MAX / 10, 5, Amount::MAX / 2 - 2);
+    }
+
+    #[test]
+    fn to_quote_error() {
+        let oracle_1 = DummyOracle::with_price(Amount::MAX / 4);
+        assert!(super::to_quote(&oracle_1, Coin::<SuperGroupTestC1>::new(8)).is_err());
+
+        let oracle_2 = DummyOracle::with_price(2);
+        assert!(
+            super::to_quote(
+                &oracle_2,
+                Coin::<SuperGroupTestC1>::new(Amount::MAX / 2 + 1),
+            )
+            .is_err()
+        );
+    }
+
+    fn assert_from_quote(oracle_price: Amount, in_amount: Amount, expected_out: Amount) {
+        let oracle = DummyOracle::with_price(oracle_price);
+
+        let out_amount = super::from_quote(&oracle, Coin::new(in_amount)).unwrap();
+
+        assert_eq!(Coin::<SuperGroupTestC1>::new(expected_out), out_amount);
+    }
+
+    fn assert_to_quote(oracle_price: Amount, in_amount: Amount, expected_out: Amount) {
+        let oracle = DummyOracle::with_price(oracle_price);
+        let out_amount =
+            super::to_quote(&oracle, Coin::<SuperGroupTestC1>::new(in_amount)).unwrap();
+        assert_eq!(Coin::<Stable>::new(expected_out), out_amount);
+    }
+}
