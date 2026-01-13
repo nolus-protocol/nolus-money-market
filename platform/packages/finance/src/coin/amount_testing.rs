5693fe818d3a6e25d611c4ff456deb831a08ad73
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2026-01-13 14:50:49 +0200

    chore(finance): Update  amount_testing comment
    Changing the TODO comment into more general description comment. The purpose of this module has changed from a temporary code to a more permanent one, which will facilitate the integration tests without sacrificing business logic and accuracy

--- platform/packages/finance/src/coin/amount_testing.rs
+++ platform/packages/finance/src/coin/amount_testing.rs
@@ -4,10 +4,10 @@ use crate::{
     percent::{Units as PercentUnits, bound::BoundPercent},
 };
 
-// TODO Remove once integration tests use BoundPercent::of(Coin)
+// These implementations exist strictly to be used for integration test purposes
+
 impl<const UPPER_BOUND: PercentUnits> CommonDoublePrimitive<BoundPercent<UPPER_BOUND>> for Amount {
     type CommonDouble = <Self as ToDoublePrimitive>::Double;
 }
 
-// TODO Remove once integration tests use BoundPercent::of(Coin)
 impl<const UPPER_BOUND: PercentUnits> Fractionable<BoundPercent<UPPER_BOUND>> for Amount {}
