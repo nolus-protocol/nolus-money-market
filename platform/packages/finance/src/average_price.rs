83d24bcb71038afe6995ac998a6cd6006b5e1ca1
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2026-01-29 12:29:45 +0200

    test(finance): AveragePrice::new()
    Co-Authored-By: Katerina Maneva <119162851+maneva3@users.noreply.github.com>

--- platform/packages/finance/src/average_price.rs
+++ platform/packages/finance/src/average_price.rs
@@ -2,10 +2,15 @@ use std::fmt::Debug;
 
 use crate::price::Price;
 
+#[cfg_attr(any(test, feature = "testing"), derive(PartialEq))]
 #[derive(Debug)]
-struct AveragePrice<C, QouteC> {
+struct AveragePrice<C, QuoteC>
+where
+    C: 'static + Debug,
+    QuoteC: 'static + Debug,
+{
     // An instance of AveragePrice will have at least one Price
-    total: Price<C, QouteC>,
+    total: Price<C, QuoteC>,
     // > 0; TODO: replace with Count
     count: u32,
 }
@@ -20,5 +25,33 @@ where
             total: initial,
             count: 1,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use currency::test::SuperGroupTestC2;
+
+    use crate::{
+        super::AveragePrice,
+        average_price::AveragePrice,
+        coin::Amount,
+        price::{self, Price, PriceBuilder},
+        test::coin,
+    };
+
+    #[test]
+    fn new() {
+        assert_eq!(
+            AveragePrice {
+                total: price(1, 2),
+                count: 1
+            },
+            AveragePrice::new(price(1, 2))
+        );
+    }
+
+    fn price(amount: Amount, quote_amount: Amount) -> Price<SuperGroupTestC2, SuperGroupTestC1> {
+        price::total_of(coin::coin2(amount)).is(coin::coin1(quote_amount))
     }
 }
