e61d9eeaa839ba918f81feb111104146ce7bc6ae
Katerina Maneva <k.g.maneva@gmail.com>
2026-02-16 18:23:33 +0200

    feat(sdk) Create custom SudoMsg and RequestPacket. Improve derive-s

--- platform/packages/sdk/src/ica.rs
+++ platform/packages/sdk/src/ica.rs
@@ -1,11 +1,10 @@
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CosmosMsg, CustomMsg};
+use cosmwasm_std::{Binary, Coin as CwCoin, CosmosMsg, CustomMsg};
 use schemars::JsonSchema;
 
 #[cfg(feature = "cosmos_proto")]
 use super::cosmos_sdk_proto::Any as CosmosAny;
-use super::cosmwasm_std::{Binary, Coin as CwCoin};
 
 /// Minimal replacement for `neutron_sdk::bindings::types::ProtobufAny`.
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
@@ -53,6 +52,15 @@ pub struct IbcFee {
     pub timeout_fee: Vec<CwCoin>,
 }
 
+// Minimal replacement for `neutron_sdk::sudo::msg::RequestPacketTimeoutHeight`.
+#[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct RequestPacketTimeoutHeight {
+    pub revision_number: Option<u64>,
+    pub revision_height: Option<u64>,
+}
+
+// Minimal replacement for `neutron_sdk::bindings::msg::NeutronMsg`.
 #[derive(Clone, Debug, Deserialize, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub enum InterChainMsg {
@@ -123,10 +131,45 @@ impl From<InterChainMsg> for CosmosMsg<InterChainMsg> {
 
 impl CustomMsg for InterChainMsg {}
 
-// Minimal replacement for `neutron_sdk::sudo::msg::RequestPacketTimeoutHeight`.
-#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize, JsonSchema)]
+// Minimal replacement for `neutron_sdk::sudo::msg::RequestPacket`.
+#[derive(Clone, Deserialize, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "snake_case")]
-pub struct RequestPacketTimeoutHeight {
-    pub revision_number: Option<u64>,
-    pub revision_height: Option<u64>,
+pub struct RequestPacket {
+    pub sequence: Option<u64>,
+    pub source_port: Option<String>,
+    pub source_channel: Option<String>,
+    pub destination_port: Option<String>,
+    pub destination_channel: Option<String>,
+    pub data: Option<Vec<u8>>,
+    pub timeout_height: Option<RequestPacketTimeoutHeight>,
+    pub timeout_timestamp: Option<u64>,
+}
+
+// Minimal replacement for `neutron_sdk::sudo::msg::SudoMsg`.
+#[derive(Clone, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum SudoMsg {
+    OpenAck {
+        port_id: String,
+        channel_id: String,
+        counterparty_channel_id: String,
+        counterparty_version: String,
+    },
+    Response {
+        request: RequestPacket,
+        data: Binary,
+    },
+    Timeout {
+        request: RequestPacket,
+    },
+    Error {
+        request: RequestPacket,
+        details: String,
+    },
+    TxQueryResult {
+        query_id: u64,
+    },
+    KVQueryResult {
+        query_id: u64,
+    },
 }
