3bd2fbbfd6b98a1ff272a695ed297927e1423fe4
Katerina Maneva <k.g.maneva@gmail.com>
2026-02-26 15:49:06 +0200

    chore(tests): Formatting

--- tests/src/common/leaser.rs
+++ tests/src/common/leaser.rs
@@ -4,7 +4,12 @@ use currencies::{LeaseGroup, Lpn, PaymentGroup};
 use currency::{CurrencyDef, MemberOf};
 use dex::{ConnectionParams, Ics20Channel, MaxSlippage};
 use finance::{
-    coin::Coin, duration::Duration, fraction::Unit, liability::Liability, percent::{Percent, Percent100}, test
+    coin::Coin,
+    duration::Duration,
+    fraction::Unit,
+    liability::Liability,
+    percent::{Percent, Percent100},
+    test,
 };
 use lease::api::{LpnCoinDTO, limits::MaxSlippages, open::PositionSpecDTO};
 use leaser::msg::{InstantiateMsg, NewConfig, QueryMsg, QuoteResponse};
