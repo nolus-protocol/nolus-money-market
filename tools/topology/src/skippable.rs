e250106ec04b90632b2f543a5db676d7757e88c6
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2026-04-14 16:44:47 +0300

    doc: Document `Skippable` type.

--- tools/topology/src/skippable.rs
+++ tools/topology/src/skippable.rs
@@ -1,5 +1,30 @@
 use serde::{Deserialize, Deserializer};
 
+/// Type for use when being able to have a field missing during
+/// deserialization, but not allowed to hold a nil value, e.g. JSON's `null`.
+///
+/// # Example
+/// ```ignore
+/// #[derive(serde::Deserialize)]
+/// struct Object {
+///   // Allowed values: 0..255
+///   required: u8,
+///   // Allowed values: 0..255
+///   // Note: acts like wrapped type when without `#[serde(default)]`.
+///   required: Skippable<u8>,
+///   // Allowed values: null, 0..255
+///   required_nullable: Option<u8>,
+///   // Allowed values: unset, 0..255
+///   #[serde(default)]
+///   skippable: Skippable<u8>,
+///   // Allowed values: unset, null, 0..255
+///   #[serde(default)]
+///   skippable_nullable: Option<u8>,
+/// }
+/// ```
+// Current uses of the type are fields akin to `icon` and `override_symbol`,
+// which aren't strictly required to be present but should not allow null as an
+// allowed value.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub(crate) enum Skippable<T> {
     #[default]
