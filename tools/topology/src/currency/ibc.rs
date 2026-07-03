3dbb5ae1105180f61d86745e691a01db377b55db
kostovster <kostovster@gmail.com>
2026-07-03 21:59:31 +0300

    refactor: order topology test module per convention
    Code Style rule 25 orders module items with mod declarations directly
    after the use statements; the test module previously sat after the impl
    block. Pure relocation, no behavioral change.

--- tools/topology/src/currency/ibc.rs
+++ tools/topology/src/currency/ibc.rs
@@ -2,35 +2,6 @@ use serde::Deserialize;
 
 use crate::{network, skippable::Skippable};
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
-#[serde(deny_unknown_fields, rename_all = "snake_case")]
-pub(crate) struct Ibc {
-    network: network::Id,
-    currency: super::Id,
-    #[serde(default)]
-    override_symbol: Skippable<super::Id>,
-}
-
-impl Ibc {
-    #[inline]
-    pub const fn network(&self) -> &network::Id {
-        &self.network
-    }
-
-    #[inline]
-    pub const fn currency(&self) -> &super::Id {
-        &self.currency
-    }
-
-    #[inline]
-    pub const fn overriden_symbol(&self) -> Option<&super::Id> {
-        match self.override_symbol {
-            Skippable::Skipped => None,
-            Skippable::Some(ref symbol) => Some(symbol),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::Ibc;
@@ -58,5 +29,34 @@ mod tests {
             .expect("Ibc without an override symbol should deserialize!");
 
         assert!(ibc.overriden_symbol().is_none(), "{ibc:?}");
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "snake_case")]
+pub(crate) struct Ibc {
+    network: network::Id,
+    currency: super::Id,
+    #[serde(default)]
+    override_symbol: Skippable<super::Id>,
+}
+
+impl Ibc {
+    #[inline]
+    pub const fn network(&self) -> &network::Id {
+        &self.network
+    }
+
+    #[inline]
+    pub const fn currency(&self) -> &super::Id {
+        &self.currency
+    }
+
+    #[inline]
+    pub const fn overriden_symbol(&self) -> Option<&super::Id> {
+        match self.override_symbol {
+            Skippable::Skipped => None,
+            Skippable::Some(ref symbol) => Some(symbol),
+        }
     }
 }
